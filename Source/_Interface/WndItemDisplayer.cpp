9b6c8233385378b026f39affef1f26dbedbfb5c5
SP SquonK <sp@sqk.fr>
2022-05-27 20:09:40 +0200

    WndItemDisplayer debug and polish

--- Source/_Interface/WndItemDisplayer.cpp
+++ Source/_Interface/WndItemDisplayer.cpp
@@ -1,13 +1,11 @@
 #include "StdAfx.h"
 #include "WndItemDisplayer.h"
 
-
-void CWndItemDisplayer::SetItem(CItemElem & item) {
+void CWndItemDisplayer::SetItem(/* const */ CItemElem & item) {
 	// Ensure that we trigger operator=
 	m_item = CItemElem();
 	*m_item = item;
-
-	texture.LoadTexture(g_Neuz.m_pd3dDevice, MakePath(DIR_ITEM, item.GetProp()->szIcon), 0xffff00ff);
+	m_item->SetTexture();
 }
 
 void CWndItemDisplayer::OnInitialUpdate() {
@@ -15,23 +13,23 @@ void CWndItemDisplayer::OnInitialUpdate() {
 	m_item = std::nullopt;
 }
 
-void CWndItemDisplayer::OnDraw(C2DRender * p2DRender) {
-	if (!m_item) return;
+void CWndItemDisplayer::OnDraw(C2DRender * const p2DRender) {
+	if (!m_item && m_defaultTooltip == NULL) return;
 
-	texture.Render(p2DRender, GetWindowRect().TopLeft(), 255);
-	// Texture1.Render(p2DRender, m_Rect[1].TopLeft(), 255);
-
+	if (m_item && m_item->m_pTexture) {
+		m_item->m_pTexture->Render(p2DRender, GetWindowRect().TopLeft(), 255);
+	}
 
 	CPoint point = GetMousePoint();
 	CRect rect = GetWindowRect();
-	if (rect.PtInRect(point)) {
-		ClientToScreen(&point);
-		ClientToScreen(&rect);
-		if (m_item) {
-			g_WndMng.PutToolTip_Item(&*m_item, point, &rect);
-		} else if (m_defaultTooltip != NULL) {
-			g_toolTip.PutToolTip(100, prj.GetText(m_defaultTooltip), rect, point, 0);
-		}
+	if (!rect.PtInRect(point)) return;
+
+	ClientToScreen(&point);
+	ClientToScreen(&rect);
+	if (m_item) {
+		g_WndMng.PutToolTip_Item(&*m_item, point, &rect);
+	} else if (m_defaultTooltip != NULL) {
+		g_toolTip.PutToolTip(100, prj.GetText(m_defaultTooltip), rect, point, 0);
 	}
 }
 
--- Source/_Interface/WndItemDisplayer.h
+++ Source/_Interface/WndItemDisplayer.h
@@ -2,22 +2,29 @@
 
 #include "WndBase.h"
 
+
+/// A CWndBase component that can display an item.
+/// 
+/// @SPSquonk, 2022-05
+/// - License: http://squonk.fr/SquonK-Hidden-Boss-License.txt
 class CWndItemDisplayer : public CWndBase {
   std::optional<CItemElem> m_item = std::nullopt;
-  CTexture texture;
   DWORD m_defaultTooltip = 0;
 
 public:
-  void SetTooltipId(DWORD tooltip) { m_defaultTooltip = tooltip; }
+  CWndItemDisplayer() {
+    m_dwStyle |= WBS_NOFRAME | WBS_CHILD | WBS_NODRAWFRAME;
+  }
+
+  // Changes the tooltip displayed when hovering the slot
+  void SetTooltipId(const DWORD tooltip) { m_defaultTooltip = tooltip; }
 
   void ResetItem() { m_item = std::nullopt; }
-  void SetItem(CItemElem & item);
+  void SetItem(/* const */ CItemElem & item);
+
+  /// Return the ID of the currently displayed item. Returns NULL if none
+  [[nodiscard]] DWORD GetItemId() const;
 
   void OnDraw(C2DRender * p2DRender) override;
-
   void OnInitialUpdate() override;
-  DWORD GetItemId() const;
-
-
 };
-
