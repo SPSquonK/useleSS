SPSquonK/useleSS

commit 179af320104b5cd7411e7572c1a6177c607b5651

CMover::IsEquipAble is const

Source/_Common/Mover.cpp
@@ -3282,7 +3282,7 @@ void CMover::Process()
 	}
 
 #ifdef __EVE_BALLOON
-	if( IsPlayer() != FALSE && HasBuffByIk3( IK3_BALLOON ) != FALSE )
+	if( IsPlayer() != FALSE && !HasBuffByIk3( IK3_BALLOON ) )
 	{
 		if( m_pBalloon == NULL )
 		{
@@ -5478,9 +5478,9 @@ BOOL CMover::IsAttackAbleNPC( CMover* pNPC )
 		{
 			if( pMoverProp->dwClass == RANK_GUARD )					// 가드일때만 성향을 비교한다.  
 			{
-				if( pMoverProp->nChaotic < 0 && IsChaotic() == TRUE )			// 성향이 서로 같으면 공격못함.
+				if( pMoverProp->nChaotic < 0 && IsChaotic() )			// 성향이 서로 같으면 공격못함.
 					bAble = FALSE;
-				else if( pMoverProp->nChaotic > 0 && IsChaotic() == FALSE )		// 성향이 서로 같으면 공격못함.
+				else if( pMoverProp->nChaotic > 0 && !IsChaotic() )		// 성향이 서로 같으면 공격못함.
 					bAble = FALSE;
 			}
 
@@ -8966,7 +8966,7 @@ void CMover::ProcessSFXExpire( void )
 #ifdef __CLIENT
 void CMover::ProcessEyeFlash()
 {
-	if( IsPlayer() && m_pModel && HasBuffByIk3(IK3_TEXT_DISGUISE) == FALSE && IsDisguise() == FALSE )
+	if( IsPlayer() && m_pModel && !HasBuffByIk3(IK3_TEXT_DISGUISE) && IsDisguise() == FALSE )
 	{
 		CModelObject* pModelObj = (CModelObject*)m_pModel;
 		

Source/_AIInterface/AIInterface.cpp
@@ -137,7 +137,7 @@ CMover* CAIInterface::ScanTargetNext( CObj* pObjCenter, int nRange, OBJID dwIdTa
 				if( D3DXVec3LengthSq( &vDist ) < fRadius && pTarget->m_idparty == uParty )
 				{
 					if( pTarget->IsMode( TRANSPARENT_MODE ) == FALSE )
-						if( pTarget->HasBuffByIk3( IK3_TEXT_DISGUISE ) == FALSE )
+						if( !pTarget->HasBuffByIk3( IK3_TEXT_DISGUISE ) )
 							return pTarget;
 				}
 			}
@@ -219,7 +219,7 @@ CMover* CAIInterface::ScanTarget( CObj* pObjCenter, int nRangeMeter, int nJobCon
 				if( D3DXVec3LengthSq( &vDist ) < fRadius )	// 두 객체간의 거리가 범위 이내이면 
 				{
 					if( pTarget->IsMode( TRANSPARENT_MODE ) == FALSE )
-						if( pTarget->HasBuffByIk3( IK3_TEXT_DISGUISE ) == FALSE )
+						if( !pTarget->HasBuffByIk3( IK3_TEXT_DISGUISE ) )
 							return pTarget;
 				}
 			}

Source/_Common/CommonCtrl.cpp
@@ -183,7 +183,7 @@ void CCommonCtrl::_ProcessWall( void )
 			CMover *pTarget = (CMover *)pObj;
 			if( pTarget->IsPeaceful() == FALSE )		// NPC가 아닌경우만 적용
 				bApply = TRUE;
-			if( pAttacker->IsPlayer() && pAttacker->IsChaotic() == FALSE && pTarget->GetProp()->dwClass == RANK_GUARD )
+			if( pAttacker->IsPlayer() && !pAttacker->IsChaotic() && pTarget->GetProp()->dwClass == RANK_GUARD )
 				bApply = FALSE;
 			if( bApply )
 			{

Source/_Common/Ctrl.cpp
@@ -328,7 +328,7 @@ void	CCtrl::ApplySkillRegion( const D3DXVECTOR3 &vPos, int nApplyType, ItemProp 
 				if( pTarget->IsPeaceful() == FALSE )		// NPC가 아닌경우만 적용
 					bApply = TRUE;
 				CMover *pAttacker = (CMover *)pSrc;
-				if( pAttacker->IsPlayer() && pAttacker->IsChaotic() == FALSE && pTarget->GetProp()->dwClass == RANK_GUARD )
+				if( pAttacker->IsPlayer() && !pAttacker->IsChaotic() && pTarget->GetProp()->dwClass == RANK_GUARD )
 					bApply = FALSE;
 			} else
 			// 대상이 컨트롤이면.
@@ -482,7 +482,7 @@ void	CCtrl::ApplySkillAround( CCtrl *pSrc, int nApplyType, ItemProp *pSkillProp,
 				if( pTarget->IsPeaceful() == FALSE )		// NPC가 아닌경우만 적용
 					bApply = TRUE;
 				CMover *pAttacker = (CMover *)pSrc;
-				if( pAttacker->IsPlayer() && pAttacker->IsChaotic() == FALSE && pTarget->GetProp()->dwClass == RANK_GUARD )
+				if( pAttacker->IsPlayer() && !pAttacker->IsChaotic() && pTarget->GetProp()->dwClass == RANK_GUARD )
 					bApply = FALSE;
 			} else
 			// 대상이 컨트롤이면.
@@ -637,7 +637,7 @@ void	CCtrl::ApplySkillLine( int nApplyType, ItemProp *pSkillProp, AddSkillProp *
 				if( pTarget->IsPeaceful() == FALSE )		// NPC가 아닌경우만 적용
 					bApply = TRUE;
 				CMover *pAttacker = (CMover *)this;
-				if( pAttacker->IsPlayer() && pAttacker->IsChaotic() == FALSE && pTarget->GetProp()->dwClass == RANK_GUARD )
+				if( pAttacker->IsPlayer() && !pAttacker->IsChaotic() && pTarget->GetProp()->dwClass == RANK_GUARD )
 					bApply = FALSE;
 			} else
 			// 대상이 컨트롤이면

Source/_Common/Mover.h
@@ -447,7 +447,7 @@ public:
 
 	BOOL	HasBuff( WORD wType, WORD wId );
 	void	RemoveBuff( WORD wType, WORD wId );
-	BOOL	HasBuffByIk3( DWORD dwIk3 );
+	[[nodiscard]] bool HasBuffByIk3(DWORD dwIk3) const;
 	BOOL	HasPet();
 #ifdef __WORLDSERVER
 	void	RemovePet();
@@ -484,8 +484,8 @@ public:
 private:
 	int		m_nTutorialState;
 public:
-	LONG	GetFlightLv( void )	{	return ( GetLevel() >= 20? 1: 0 );		}
-	void	SetFlightLv( LONG nFlightLv )	{}
+	[[nodiscard]] LONG	GetFlightLv() const	{	return ( GetLevel() >= 20? 1: 0 );		}
+	[[depecrated]] void	SetFlightLv( LONG nFlightLv )	{}
 	int		GetTutorialState( void )	{	return m_nTutorialState;	}
 	void	SetTutorialState( int nTutorialState )	{	m_nTutorialState	= nTutorialState;	}
 
@@ -692,8 +692,8 @@ public:
 #endif	// __WORLDSERVER
 	BOOL	IsUsingEatPet( CItemElem* pItemElem );	// 사용중인 먹펫인가?
 
-	BOOL	HasActivatedEatPet( void )		{	return m_oiEatPet != NULL_ID;	}
-	BOOL	HasActivatedSystemPet( void )	{	return m_dwPetId != NULL_ID;	}
+	[[nodiscard]] bool HasActivatedEatPet() const noexcept    { return m_oiEatPet != NULL_ID; }
+	[[nodiscard]] bool HasActivatedSystemPet() const noexcept { return m_dwPetId != NULL_ID; }
 	OBJID	GetEatPetId( void )		{	return m_oiEatPet;		}
 	void	SetEatPetId( OBJID oiEatPet )		{	m_oiEatPet	= oiEatPet;	}
 	DWORD	GetPetId( void )	{	return m_dwPetId;	}
@@ -913,7 +913,31 @@ public:
 	void InvalidEquipOff(BOOL fFakeparts);		// 거시기한 장비를 벗김.
 #endif
 	BOOL			DoEquip( CItemElem* pItemElem, BOOL bEquip, int nPart = -1 ); // for normal
-	BOOL			IsEquipAble( CItemElem* pItem,BOOL bIgnoreLevel = FALSE );		// 장착가능한가?
+	
+	struct EquipAble {
+		struct Yes { Yes() = default; };
+		struct No {
+		private:
+			std::optional<DWORD> tooltip;
+			CString andString;
+
+		public:
+			No() : tooltip(std::nullopt) {}
+			No(DWORD tooltip) : tooltip(tooltip) {}
+
+			template<typename ... Ts>
+			No(DWORD tooltip, const char * format, const Ts & ... ts)
+				: tooltip(tooltip) {
+				andString.Format(format, ts...);
+			}
+
+			void Display(CMover & mover) const;
+		};
+
+		using Result = std::variant<Yes, No>;
+	};
+	
+	[[nodiscard]] EquipAble::Result IsEquipAble(const CItemElem & pItem, bool bIgnoreLevel = false) const;		// 장착가능한가?
 	[[nodiscard]] bool IsUnEquipAble(const ItemProp & pItemProp) const; // Check if the current situation blocks equiping the item
 	void			SetEquipDstParam();
 	void			SumEquipAdjValue( int* pnAdjHitRate, int* pnAdjParray );
@@ -1096,11 +1120,11 @@ public:
 	void			SetPKPink( DWORD dwTime ) { if( dwTime == 0 || m_dwPKTime < dwTime ) m_dwPKTime = dwTime; }
 	DWORD			GetPKPink( void ) { return m_dwPKTime; }
 	BOOL			IsPKPink( void )	{ return m_dwPKTime > 0; }			/// PK 핑크 모드인지
-	BOOL			IsChaotic( void ) { return m_dwPKPropensity > 0; }	/// 카오인지
+	[[nodiscard]] BOOL IsChaotic() const { return m_dwPKPropensity > 0; }	/// 카오인지
 	void			SetPKValue( int nValue );
 	int				GetPKValue( void ) { return m_nPKValue; }
 	void			SetPKPropensity( DWORD dwValue );
-	DWORD			GetPKPropensity( void ) { return m_dwPKPropensity; }
+	[[nodiscard]] DWORD GetPKPropensity() const noexcept { return m_dwPKPropensity; }
 	DWORD			NextPKPropensity( int nPKValue );
 	float			GetResist(SAI79::ePropType p_PropType);
 	[[nodiscard]] int GetSetItemParts(DWORD dwParts) const;

Source/_Common/MoverEquip.cpp
@@ -1009,13 +1009,37 @@ BOOL CMover::DoEquip( int nSex, int nSkinSet,
 	return TRUE;
 }
 
+
+void CMover::EquipAble::No::Display(CMover & mover) const {
+	if (!tooltip) return;
+
+#ifdef __WORLDSERVER
+	if (mover.IsPlayer()) {
+		CUser & user = static_cast<CUser &>(mover);
+
+		if (andString.GetLength() == 0) {
+			user.AddDefinedText(tooltip.value());
+		} else {
+			static_cast<CUser &>(mover).SendSnapshotThisId<
+				SNAPSHOTTYPE_DEFINEDTEXT, DWORD, const char *
+			>(
+				tooltip.value(), andString.GetString()
+				);
+		}
+	}
+#endif
+#ifdef __CLIENT
+	if (andString.GetLength() == 0) {
+		g_WndMng.PutString(tooltip.value());
+	}
+#endif
+}
+
 // 장착 가능한가를 검사.
-BOOL CMover::IsEquipAble( CItemElem* pItem,BOOL bIgnoreLevel )
-{
-	if( !pItem )
-		return FALSE;
-	ItemProp* pItemProp	= pItem->GetProp();
-	OBJID dwObjid	= pItem->m_dwObjId;
+CMover::EquipAble::Result CMover::IsEquipAble(const CItemElem & pItem, bool bIgnoreLevel) const {
+	const ItemProp * const pItemProp	= pItem.GetProp();
+	const OBJID dwObjid	= pItem.m_dwObjId;
+
 	// 빗자루류 검사.
 	if( pItemProp->dwParts == PARTS_RIDE )
 	{
@@ -1032,64 +1056,42 @@ BOOL CMover::IsEquipAble( CItemElem* pItem,BOOL bIgnoreLevel )
 		{
 			int nAttr	= pWorld->GetHeightAttribute( GetPos().x, GetPos().z );
 			
-			if( GetFlightLv() < nLimitLv )		// 비행레벨이 안되면 못탄다.
-			{
-#ifdef __WORLDSERVER
-				if( TRUE == IsPlayer() )
-					( (CUser*)this )->AddDefinedText( TID_GAME_USEAIRCRAFT, "" );	// 잠시만 기다려주세요.
-#else // __WORLDSERVER
-				g_WndMng.PutString( prj.GetText( TID_GAME_USEAIRCRAFT ), NULL, prj.GetTextColor( TID_GAME_USEAIRCRAFT ) );
-#endif // __WORLDSERVER
-				return FALSE;
+			if (GetFlightLv() < nLimitLv) {		// 비행레벨이 안되면 못탄다.
+				return EquipAble::No(TID_GAME_USEAIRCRAFT);
 			}
 			
-			if( !pWorld->m_bFly )
-			{
-#ifdef __WORLDSERVER
-				( (CUser*)this )->AddDefinedText( TID_ERROR_NOFLY, "" );	// 비행금지구역입니다.
-#else // __WORLDSERVER
-				g_WndMng.PutString( prj.GetText( TID_ERROR_NOFLY ), NULL, prj.GetTextColor( TID_ERROR_NOFLY ) );
-#endif // __WORLDSERVER
-				return FALSE;
+			if (!pWorld->m_bFly) {
+				// 비행금지구역입니다.
+				return EquipAble::No(TID_ERROR_NOFLY);
 			}
 
-			if( HasBuffByIk3( IK3_TEXT_DISGUISE ) )
-			{
-#ifdef __WORLDSERVER
-				( (CUser*)this )->AddDefinedText( TID_QUEST_DISQUISE_NOTFLY, "" );	// 변신중에는 비행을 할수 없습니다
-#else // __WORLDSERVER
-				g_WndMng.PutString( prj.GetText( TID_QUEST_DISQUISE_NOTFLY ), NULL, prj.GetTextColor( TID_QUEST_DISQUISE_NOTFLY ) );
-#endif // __WORLDSERVER
-				return FALSE;
+			if (HasBuffByIk3(IK3_TEXT_DISGUISE)) {
+				// 변신중에는 비행을 할수 없습니다
+				return EquipAble::No(TID_QUEST_DISQUISE_NOTFLY);
 			}
 			
 			// 장착 이전에 검사하여야 한다.
 			if( nAttr == HATTR_NOFLY )		// 비행금지구역에선 못탄다.
-				return FALSE;
+				return EquipAble::No();
 			
 		#ifdef __WORLDSERVER
 			if( m_pActMover->IsState( OBJSTA_STAND ) == FALSE )
-				return FALSE;
+				return EquipAble::No();
 		#endif 
 		}
 
 #ifdef __WORLDSERVER
-		if( IsChaotic() )
-		{
-			CHAO_PROPENSITY Propensity = prj.GetPropensityPenalty( GetPKPropensity() );
-			if( !Propensity.nFly )
-			{
-				((CUser*)this)->AddDefinedText( TID_GAME_CHAOTIC_NOT_FLY );
-				return FALSE;
+		if (IsChaotic()) {
+			CHAO_PROPENSITY Propensity = prj.GetPropensityPenalty(GetPKPropensity());
+			if (!Propensity.nFly) {
+				return EquipAble::No(TID_GAME_CHAOTIC_NOT_FLY);
 			}
 		}
 #endif // __WORLDSERVER
 
 #ifdef __WORLDSERVER
-		if( HasActivatedEatPet() || HasActivatedSystemPet() )	// 펫이 소환된 상태라면 비행 불가
-		{
-			( (CUser*)this )->AddDefinedText( TID_GAME_CANNOT_FLY_WITH_PET );
-			return FALSE;
+		if (HasActivatedEatPet() || HasActivatedSystemPet()) { // 펫이 소환된 상태라면 비행 불가
+			return EquipAble::No(TID_GAME_CANNOT_FLY_WITH_PET);
 		}
 #endif	// __WORLDSERVER
 	}
@@ -1099,11 +1101,7 @@ BOOL CMover::IsEquipAble( CItemElem* pItem,BOOL bIgnoreLevel )
 		// 성별 확인( 무기는 빠짐 )
 		if( pItemProp->dwItemSex != NULL_ID && pItemProp->dwItemSex != GetSex() )
 		{
-#ifdef __WORLDSERVER
-			if( TRUE == IsPlayer() )
-				( (CUser*)this )->AddDefinedText( TID_GAME_WRONGSEX, "\"%s\"", pItemProp->szName );
-#endif	// __WORLDSERVER
-			return FALSE;
+			return EquipAble::No(TID_GAME_WRONGSEX, "\"%s\"", pItemProp->szName);
 		} // 성별
 	}// 무기
 	
@@ -1111,11 +1109,7 @@ BOOL CMover::IsEquipAble( CItemElem* pItem,BOOL bIgnoreLevel )
 	if( pItemProp->dwItemKind1 != IK1_WEAPON )
 	{
 		if( pItemProp->dwItemJob != NULL_ID && !IsInteriorityJob( pItemProp->dwItemJob ) ) {
-#ifdef __WORLDSERVER
-			if( TRUE == IsPlayer() )
-				( (CUser*)this )->AddDefinedText( TID_GAME_WRONGJOB, "\"%s\"", pItemProp->szName );
-#endif	// __WORLDSERVER
-			return FALSE;
+			return EquipAble::No(TID_GAME_WRONGJOB, "\"%s\"", pItemProp->szName);
 		}
 	}
 
@@ -1124,49 +1118,26 @@ BOOL CMover::IsEquipAble( CItemElem* pItem,BOOL bIgnoreLevel )
 		// 아이템 필요레벨 검사.
 		if( pItemProp->dwLimitLevel1 != 0xffffffff )	// 필요레벨이 지정되어 있고
 		{
-			if( pItem->IsLimitLevel( this ) )	// 레벨이 안되면 못참.
+			if( pItem.IsLimitLevel( this ) )	// 레벨이 안되면 못참.
 			{
-#	ifdef __WORLDSERVER
-				if( TRUE == IsPlayer() )
-				{
-					( (CUser*)this )->AddDefinedText( TID_GAME_REQLEVEL, "\"%d\"", pItem->GetLimitLevel() );
-				}
-#	endif	// __WORLDSERVER
-				return FALSE;
-			}
-		}
-		// 보석류를 장착
-		if( pItemProp->dwItemKind2 == IK2_JEWELRY )
-		{
-			if( pItemProp->dwLimitLevel1 != 0xffffffff )	// 제한레벨이 걸려있고
-			{
-				if( pItem->IsLimitLevel( this ) )	// 레벨이 안되면 못참.
-				{
-#	ifdef __WORLDSERVER
-					if( TRUE == IsPlayer() )
-					{
-						( (CUser*)this )->AddDefinedText( TID_GAME_REQLEVEL, "\"%d\"", pItem->GetLimitLevel() );
-					}
-#	endif	// __WORLDSERVER
-					return FALSE;
-				}
+				return EquipAble::No(TID_GAME_REQLEVEL, "\"%d\"", pItem.GetLimitLevel());
 			}
 		}
 
 	}
 
-	ItemProp *pHandItemProp	= NULL;
+	const ItemProp * pHandItemProp	= NULL;
 	
 	// 들고있는 무기 프로퍼티 꺼냄.
-	CItemElem *pItemElem = m_Inventory.GetEquip( PARTS_RWEAPON );		
+	const CItemElem * pItemElem = m_Inventory.GetEquip( PARTS_RWEAPON );		
 	if( pItemElem )
 		pHandItemProp = pItemElem->GetProp();
 
 	// 화살을 착용하려 했을때 보우일때만 착용돼야 함
 	if( pItemProp->dwItemKind3 == IK3_ARROW && ( pHandItemProp == NULL || pHandItemProp->dwItemKind3 != IK3_BOW ) )
-		return FALSE;			
+		return EquipAble::No();
 
-	return TRUE;
+	return EquipAble::Yes();
 }
 
 //
@@ -1213,8 +1184,14 @@ BOOL CMover::DoEquip( CItemElem* pItemElem, BOOL bEquip, int nPart )
 			return FALSE;
 		if( pItemProp->dwParts == PARTS_RIDE && pItemElem && pItemElem->IsFlag( CItemElem::expired ) )
 			return FALSE;
-		if( IsEquipAble( pItemElem ) == FALSE )
+
+		const EquipAble::Result result = IsEquipAble(*pItemElem);
+		if (const EquipAble::No * const no = std::get_if<EquipAble::No>(&result)) {
+			no->Display(*this);
 			return FALSE;
+		}
+
+		static_assert(std::variant_size_v<EquipAble::Result> == 2);
 	}
 	else
 	{
@@ -1533,7 +1510,7 @@ void CMover::InvalidEquipOff(BOOL bFakeParts) {
 		const ItemProp * pItemProp = pItemElem ? pItemElem->GetProp() : nullptr;
 		if (!pItemProp) continue;	// 프로퍼티 없으면 실패.
 
-		if (!IsEquipAble(pItemElem, TRUE)) {	// 장착할 수 없는 아이템이다.
+		if (!std::holds_alternative<EquipAble::Yes>(IsEquipAble(*pItemElem, true))) {	// 장착할 수 없는 아이템이다.
 			if (!DoEquip(pItemElem, FALSE)) {	// 벗김.
 				// 벗기는데 실패함. 인벤이 꽉찼다거나 기타등등 이유
 				Error("아템 벗기는데 실패:%s", GetName());

Source/_Common/MoverRender.cpp
@@ -750,7 +750,7 @@ void CMover::Render( LPDIRECT3DDEVICE9 pd3dDevice )
 	if( m_pModel->m_nNoEffect == 0 )
 		RenderQuestEmoticon( pd3dDevice );
 
-	if( IsActiveMover() && m_pModel && IsDie() && HasBuffByIk3(IK3_TEXT_DISGUISE) == FALSE )
+	if( IsActiveMover() && m_pModel && IsDie() && !HasBuffByIk3(IK3_TEXT_DISGUISE) )
 	{
 		CModelObject* pModelObj = (CModelObject*)m_pModel;
 		

Source/_Common/buff.cpp
@@ -699,9 +699,11 @@ IBuff* CBuffMgr::GetBuffByIk3( DWORD dwIk3 )
 	return NULL;
 }
 
-BOOL CBuffMgr::HasBuffByIk3( DWORD dwIk3 )
-{
-	return GetBuffByIk3( dwIk3 ) != NULL;
+bool CBuffMgr::HasBuffByIk3(const DWORD dwIk3) const {
+	return std::ranges::any_of(
+		m_mapBuffs | std::views::values,
+		[dwIk3](IBuff * pBuff) { return pBuff->IsIk3(dwIk3); }
+	);
 }
 
 BOOL CBuffMgr::AddBuff( WORD wType, WORD wId, DWORD dwLevel, DWORD tmTotal, OBJID oiAttacker )

commit 67af8e6626736cd878a9363c9175c38c5f2f02d7

Fix has balloon check bug in process introduced in 179af320104b5cd7411e7572c1a6177c607b5651

Source/_Common/Mover.cpp
@@ -3219,7 +3219,7 @@ void CMover::Process()
 	}
 
 #ifdef __EVE_BALLOON
-	if( IsPlayer() != FALSE && !HasBuffByIk3( IK3_BALLOON ) )
+	if( IsPlayer() != FALSE && HasBuffByIk3( IK3_BALLOON ) )
 	{
 		if( m_pBalloon == NULL )
 		{