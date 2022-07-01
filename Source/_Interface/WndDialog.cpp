SPSquonK/useleSS

commit 7c3eac8ce1793d07f7ecb23753ab2188da22a494

CWndDialog subclasses are in the class

Source/_Interface/WndDialog.cpp
@@ -16,33 +16,16 @@
   WndId : APP_DIALOG - Dialog
 ****************************************************/
 
-CWndDialog::CWndDialog() 
-{ 
-	m_nWordButtonNum = 0;
-	m_nKeyButtonNum = 0;
-	m_nContextButtonNum = 0;
-	m_nNewQuestListNumber = 0;
-	m_nCurrentQuestListNumber = 0;
-	m_nSelectKeyButton = -1;
-	m_pNewQuestListIconTexture = NULL;
-	m_pExpectedQuestListIconTexture = NULL;
-	m_pCurrentQuestListIconTexture = NULL;
-	m_pCompleteQuestListIconTexture = NULL;
-	m_bWordButtonEnable = FALSE;
-	m_dwQuest = 0;
-	ZeroMemory( m_apWndAnswer, sizeof( m_apWndAnswer ) );
-} 
 CWndDialog::~CWndDialog() 
 { 
-	for( int i = 0; i < m_strArray.GetSize(); i++ )
-	{
-		CEditString* pEditString = (CEditString*) m_strArray.GetAt( i );
-		safe_delete( pEditString );
+	for (int i = 0; i < m_strArray.GetSize(); i++) {
+		CEditString * pEditString = (CEditString *)m_strArray.GetAt(i);
+		safe_delete(pEditString);
 	}
-	for( int i = 0; i < 6; i++ )
-		SAFE_DELETE( m_apWndAnswer[ i ] )
-	CWndQuest* pWndQuest = (CWndQuest*)g_WndMng.GetWndBase( APP_QUEST_EX_LIST );
-	if( pWndQuest ) pWndQuest->Update();
+	
+	if (CWndQuest * pWndQuest = CWndBase::GetWndBase<CWndQuest>(APP_QUEST_EX_LIST)) {
+		pWndQuest->Update();
+	}
 } 
 BOOL CWndDialog::OnSetCursor( CWndBase* pWndBase, UINT nHitTest, UINT message )
 {
@@ -798,8 +781,9 @@ void CWndDialog::MakeAnswerButton()
 		CString strTexture;
 		int nWndId = 0;
 		int j = 0;
-		for( int i = 0; i < 6; i++ )
-			SAFE_DELETE( m_apWndAnswer[ i ] )
+
+		std::ranges::generate(m_apWndAnswer, [] { return nullptr; });
+
 		for( int i = 0; i < m_nWordButtonNum; i++ )
 		{
 			WORDBUTTON* pWordButton = &m_aWordButton[ i ];
@@ -829,11 +813,11 @@ void CWndDialog::MakeAnswerButton()
 			}
 			if(	nWndId )
 			{
-				m_apWndAnswer[ j ] = new CWndAnswer;
+				m_apWndAnswer[ j ] = std::make_unique<CWndAnswer>();
 				m_apWndAnswer[ j ]->Create( "", WBS_CHILD, rect, this, nWndId );
 				m_apWndAnswer[ j ]->SetTexture( D3DDEVICE, MakePath( DIR_THEME, strTexture ), 1 );
 				m_apWndAnswer[ j ]->FitTextureSize();
-				m_apWndAnswer[ j ]->m_pWordButton = (WORDBUTTON*)	pWordButton;
+				m_apWndAnswer[ j ]->m_pWordButton = pWordButton;
 				j++;
 			}
 			x += 90;
@@ -879,7 +863,7 @@ BOOL CWndDialog::OnChildNotify( UINT message, UINT nID, LRESULT* pLResult )
 	case WIDC_CANCEL:
 		{
 			CWndAnswer* pWndAnswer = (CWndAnswer*)pLResult;
-			WORDBUTTON* pWordButton = (WORDBUTTON*)pWndAnswer->m_pWordButton;
+			const WORDBUTTON* pWordButton = pWndAnswer->m_pWordButton;
 			BeginText();
 			RunScript( pWordButton->szKey,  pWordButton->dwParam,  pWordButton->dwParam2 );
 			MakeKeyButton();

Source/_Interface/WndDialog.h
@@ -1,42 +1,46 @@
-#ifndef __WNDDIALOG2__H
-#define __WNDDIALOG2__H
+#pragma once
 
-class CWndAnswer : public CWndButton
-{
-public:
-	LPVOID m_pWordButton;
-};
+#include <array>
 
 class CWndDialog : public CWndNeuz 
 { 
-	CWndAnswer* m_apWndAnswer[ 6 ];
+public:
+	struct WORDBUTTON {
+		BOOL bStatus;
+		CRect rect;
+		TCHAR szWord[64];
+		TCHAR szKey[64];
+		DWORD dwParam;
+		DWORD dwParam2;
+		int nLinkIndex; // Index to concatenate if word is broken by a newline
+	};
+
+	class CWndAnswer : public CWndButton {
+	public:
+		const WORDBUTTON * m_pWordButton;
+	};
+
+private:
+	std::array<std::unique_ptr<CWndAnswer>, 6> m_apWndAnswer;
+
 public: 
 	CTimer m_timer;
 	CTexture m_texChar;
-	BOOL m_bWordButtonEnable;
-	int m_nWordButtonNum;
-	int m_nKeyButtonNum;
-	int m_nContextButtonNum;
-	int m_nNewQuestListNumber;
-	int m_nCurrentQuestListNumber;
-	int m_nSelectKeyButton;
+	BOOL m_bWordButtonEnable = FALSE;
+	int m_nWordButtonNum = 0;
+	int m_nKeyButtonNum  = 0;
+	int m_nContextButtonNum = 0;
+	int m_nNewQuestListNumber = 0;
+	int m_nCurrentQuestListNumber = 0;
+	int m_nSelectKeyButton = - 1;
 	CUIntArray m_aContextMark[ 32 ];
-	struct WORDBUTTON 
-	{
-		BOOL bStatus;
-		CRect rect; 
-		TCHAR szWord[ 64 ];
-		TCHAR szKey[ 64 ];
-		DWORD dwParam;
-		DWORD dwParam2;
-		int nLinkIndex; // 줄바꿈으로 단어가 끊어진 경우 연결하기 위한 인덱스 
-	};
+
 	WORDBUTTON m_aWordButton[ 32 ];
 	WORDBUTTON m_aKeyButton[ 32 ];
 	WORDBUTTON m_aContextButton[ 32 ];
 	CEditString m_string;
 
-	DWORD m_dwQuest;// context 버튼에서 사용함 
+	DWORD m_dwQuest = 0;// context 버튼에서 사용함 
 	BOOL m_bSay;
 	int m_nCurArray;
 	CPtrArray m_strArray;
@@ -45,13 +49,13 @@ public:
 private:
 	CWndListBox m_WndNewQuestListBox;
 	CWndListBox m_WndCurrentQuestListBox;
-	CTexture* m_pNewQuestListIconTexture;
-	CTexture* m_pExpectedQuestListIconTexture;
-	CTexture* m_pCurrentQuestListIconTexture;
-	CTexture* m_pCompleteQuestListIconTexture;
+	CTexture* m_pNewQuestListIconTexture = nullptr;
+	CTexture* m_pExpectedQuestListIconTexture = nullptr;
+	CTexture* m_pCurrentQuestListIconTexture = nullptr;
+	CTexture* m_pCompleteQuestListIconTexture = nullptr;
 public:
 	 
-	CWndDialog(); 
+	CWndDialog() = default;
 	~CWndDialog(); 
 
 	void RemoveAllKeyButton();
@@ -90,4 +94,4 @@ public:
 	virtual void OnLButtonDown( UINT nFlags, CPoint point ); 
 	virtual void OnMouseWndSurface( CPoint point);
 }; 
-#endif
+

commit 5201d918db200545cc27bdbd50b94e4b7af99dce

Fix crash introduced in 7c3eac8ce1793d07f7ecb23753ab2188da22a494
- Crash occured for example when refusing a quest from an NPC (an probably)
- Because CWndBase::Destroy(TRUE) will take care later of deleting the widget,
so we should not delete it

Source/_Interface/WndDialog.cpp
@@ -538,7 +538,7 @@ void CWndDialog::BeginText()
 	{
 		if( m_apWndAnswer[ i ] )
 			m_apWndAnswer[ i ]->Destroy( TRUE );
-		m_apWndAnswer[ i ] = NULL;
+		[[maybe_unused]] CWndBase * willBeDeletedByDestroy = m_apWndAnswer[ i ].release();
 	}
 }
 void CWndDialog::MakeContextButton()