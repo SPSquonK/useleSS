9c781bb57f8ad2f77f1388c2f62b66503f794877
SP SquonK <sp@sqk.fr>
2022-08-12 02:04:19 +0200

    ForEachPower method in CWndGuildTabPower

--- Source/_Interface/WndGuildTabPower.h
+++ Source/_Interface/WndGuildTabPower.h
@@ -1,32 +1,23 @@
-
-//Author : gmpbigsun
-//Date : 2009_11_16
-// Tab winodw ( of guild window )
-
 #pragma once 
 
+#include <concepts>
 
-class CWndGuildTabPower : public CWndNeuz 
-{ 
+class CWndGuildTabPower : public CWndNeuz {
 	// 길드윈도우에서 추가되는 탭 윈도우 ( 길드 하우스에 관한 권한 설정 )
-public: 
-	CWndGuildTabPower(); 
-	virtual ~CWndGuildTabPower(); 
-
-	virtual BOOL Initialize( CWndBase* pWndParent = NULL, DWORD nType = MB_OK ); 
-	virtual void OnDraw( C2DRender* p2DRender ); 
-	virtual	void OnInitialUpdate(); 
-	virtual BOOL OnCommand( UINT nID, DWORD dwMessage, CWndBase* pWndBase ); 
-	virtual void OnSize( UINT nType, int cx, int cy ); 
-	virtual BOOL OnChildNotify( UINT message, UINT nID, LRESULT* pLResult );
+public:
+	BOOL Initialize(CWndBase * pWndParent = NULL, DWORD nType = MB_OK) override;
+	void OnInitialUpdate() override;
+	BOOL OnChildNotify(UINT message, UINT nID, LRESULT * pLResult) override;
 
 	void UpdateData();
-	void SetData( DWORD dwPower[] );
+	void SetData(DWORD dwPower[]);
 	void EnableButton(BOOL bEnable);
 
-protected:
-	DWORD m_adwPower[ MAX_GM_LEVEL ];
-	BOOL m_bChanedCheckBox;
+private:
+	DWORD m_adwPower[MAX_GM_LEVEL];
+	BOOL m_bChanedCheckBox = FALSE;
+	bool m_hasBeenChanged = false;
 
-}; 
-
+	void ForEachPower(std::invocable<UINT, int, DWORD> auto func);
+	void ForEachPower(std::invocable<CWndButton &, int, DWORD> auto func);
+};
