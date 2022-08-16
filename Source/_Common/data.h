34afc7a6fb55844534c3ec7ccba2a9c6adedf47c
SP SquonK <sp@sqk.fr>
2022-08-16 02:38:08 +0200

    Translate data.h

--- Source/_Common/data.h
+++ Source/_Common/data.h
@@ -2,37 +2,38 @@
 
 #include "debug.h"
 
-const DWORD NULL_ID	= 0xffffffff;
+static constexpr DWORD NULL_ID	= 0xffffffff;
 
 #include "Container_FixedArray.hpp"
 
-typedef	__int64	EXPINTEGER;
-typedef	double	EXPFLOAT;
+using EXPINTEGER = __int64;
+using EXPFLOAT   = double;
 
 #if defined(__WORLDSERVER ) || defined(__CLIENT)
-struct NaviPoint //	네비게이션에 클릭으로 위치를 표시하는 기능을 위한 구조체
-{
-	D3DXVECTOR3 Pos = D3DXVECTOR3(0.f, 0.f, 0.f);		//	이 위치는 네이게이터상의 위치가 아닌 월드상의 위치임을 강조한다.
-	short		Time = 0;			//	포인트의 남아있는 시간
+// A struct for the ability to mark a location on click in the navigation.
+struct NaviPoint {
+	// Emphasize that this location is a location in the world, not a location on the navigator.
+	D3DXVECTOR3 Pos = D3DXVECTOR3(0.f, 0.f, 0.f);
+	// Time remaining in points
+	short		Time = 0;
 	DWORD		objid = 0xffffffff;
 	std::string      Name = "";
 }; 
 
 typedef std::vector<NaviPoint> V_NaviPoint;
 
-#endif // defined(__WORLDSERVER ) || defined(__CLIENT)
+#endif
 
 namespace SAI79
 {
-	// ************************************
-	// * 타입 없음.	: 0
-	// * 불   속성	: 1
-	// * 물   속성	: 2
-	// * 전기 속성	: 3
-	// * 바람 속성	: 4
-	// * 땅   속성	: 5
-	// * 이   속성 타입을 ePropType로 설정
-	// ************************************
-	enum	ePropType 	{ NO_PROP = 0, FIRE, WATER, ELECTRICITY, WIND, EARTH, END_PROP };
-} // namespace SAI79
+	enum	ePropType {
+		NO_PROP = 0,
+		FIRE,
+		WATER,
+		ELECTRICITY,
+		WIND,
+		EARTH,
+		END_PROP
+	};
+}
 
