SPSquonK/useleSS

commit 0065afdc2480fd2b368520d8e9ec593a96ef4c0c

[Taxes] Less typedef

Source/_Common/Tax.cpp
@@ -151,7 +151,7 @@ BOOL CTax::IsApplyTaxRate( const CMover* pMover, const CItemElem* pItemElem ) co
 			return FALSE;
 	}
 
-	return TRUE;
+	return FALSE; // "oops rich people are not going to steal money from new players"
 }
 
 #endif // !__DBSERVER
@@ -476,7 +476,7 @@ void CTax::SendSetTaxRateOpenWnd( BYTE nCont, DWORD dwGuildId )		// 점령길드에게
 	CGuild* pGuild = g_GuildMng.GetGuild( dwGuildId );
 	if( pGuild )
 	{
-		CUser* pUserTemp = (CUser*)prj.GetUserByID( pGuild->m_idMaster );
+		CUser* pUserTemp = prj.GetUserByID( pGuild->m_idMaster );
 		if( IsValidObj( pUserTemp ) )
 			pUserTemp->AddTaxSetTaxRateOpenWnd( nCont );
 	}
@@ -487,12 +487,11 @@ void CTax::SendNoSetTaxRateOpenWnd( CUser* pUser )	// 세율 설정을 하지 않은경우 
 	CGuild* pGuild = pUser->GetGuild();
 	if( pGuild )
 	{
-		for( TAXINFOMAP::iterator it=m_mapTaxInfo.begin(); it!=m_mapTaxInfo.end(); it++ )
-		{
-			if( it->first != CONT_ALL && it->second->dwNextId == pGuild->GetGuildId()
-				&& !it->second->bSetTaxRate && pGuild->IsMaster( pUser->m_idPlayer ) )
+		for (const auto & [contId, taxInfo] : m_mapTaxInfo) {
+			if(contId != CONT_ALL && taxInfo->dwNextId == pGuild->GetGuildId()
+				&& !taxInfo->bSetTaxRate && pGuild->IsMaster( pUser->m_idPlayer ) )
 			{
-				pUser->AddTaxSetTaxRateOpenWnd( it->first );
+				pUser->AddTaxSetTaxRateOpenWnd(contId);
 				return;
 			}
 		}
@@ -767,7 +766,7 @@ void CTaxDBController::LoadTaxInfo()
 		while( pQuery->Fetch() )
 		{
 			BYTE nTaxKind = static_cast<BYTE>( pQuery->GetInt( "nTaxKind" ) );
-			if( taxInfo->mapTaxDetail.find(nTaxKind) == taxInfo->mapTaxDetail.end() )
+			if( taxInfo->mapTaxDetail.contains(nTaxKind) )
 			{
 				WriteLog( "LoadTaxInfo() - taxDetail is wrong, nContinent = %d, nTaxKind = %d", nContinent, nTaxKind );
 				return;

Source/_Common/Tax.h
@@ -39,46 +39,29 @@ private:
 };
 #endif // __DBSERVER
 
-
 struct __TAXDETAIL
 {
-	int nTaxRate;
+	int nTaxRate = 0;
 #ifdef __DBSERVER
-	int nTaxCount;
-	int	nTaxGold;
-	int nTaxPerin;
-	int	nNextTaxRate;
+	int nTaxCount = 0;
+	int	nTaxGold = 0;
+	int nTaxPerin = 0;
+	int	nNextTaxRate = 0;
 #endif // __DBSERVER
-	__TAXDETAIL()
-	{
-		nTaxRate = 0;
-#ifdef __DBSERVER
-		nTaxCount = 0;
-		nTaxGold = 0;
-		nTaxPerin = 0;
-		nNextTaxRate = 0;
-#endif // __DBSERVER
-	}
 };
-typedef std::map<BYTE, __TAXDETAIL*> TAXDETAILMAP;
 
 struct __TAXINFO
 {
-	DWORD	dwId;
-	BOOL	bSetTaxRate;
-	TAXDETAILMAP mapTaxDetail;
-	DWORD	dwNextId;
+	DWORD	dwId = NULL_ID;
+	BOOL	bSetTaxRate = TRUE;
+	std::map<BYTE, __TAXDETAIL *> mapTaxDetail;
+	DWORD	dwNextId = NULL_ID;
 	__TAXINFO()
 	{
-		dwId = NULL_ID;
-		bSetTaxRate = TRUE;
 		mapTaxDetail.emplace(TAX_SALES, new __TAXDETAIL );			// 구매 세금은 기본
 		mapTaxDetail.emplace(TAX_PURCHASE, new __TAXDETAIL );		// 판매 세금은 기본
-		dwNextId = NULL_ID;
 	}
 };
-typedef std::map<BYTE, __TAXINFO*> TAXINFOMAP;
-
 
 class CTax
 {
@@ -93,7 +76,7 @@ public:
 	int m_nMinTaxRate;					// 최소 세율
 	int m_nMaxTaxRate;					// 최대 세율
 
-	TAXINFOMAP m_mapTaxInfo;	// 각 대륙별 점령길드 세율 정보 및 세금 수입
+	std::map<BYTE, __TAXINFO *> m_mapTaxInfo;	// 각 대륙별 점령길드 세율 정보 및 세금 수입
 
 	__TAXINFO* GetTaxInfo( BYTE nContinent );
 	void SetNextSecretRoomGuild( BYTE nCont, DWORD dwGuildId );

commit d6dfdcd2c7921e6540124126d3f673fc290810a1

Fix tax loading glitch introduced in 0065afdc2480fd2b368520d8e9ec593a96ef4c0c - Closes #89

Source/_Common/Tax.cpp
@@ -761,14 +761,16 @@ void CTaxDBController::LoadTaxInfo()
 		{ WriteLog( "%s, %d\t%s", __FILE__, __LINE__, szQuery ); return; }
 		while( pQuery->Fetch() )
 		{
-			BYTE nTaxKind = static_cast<BYTE>( pQuery->GetInt( "nTaxKind" ) );
-			if( taxInfo->mapTaxDetail.contains(nTaxKind) )
-			{
-				WriteLog( "LoadTaxInfo() - taxDetail is wrong, nContinent = %d, nTaxKind = %d", nContinent, nTaxKind );
+			BYTE nTaxKind = static_cast<BYTE>(pQuery->GetInt("nTaxKind"));
+
+			const auto itTaxDetail = taxInfo->mapTaxDetail.find(nTaxKind);
+
+			if (itTaxDetail == taxInfo->mapTaxDetail.end()) {
+				WriteLog("LoadTaxInfo() - taxDetail is wrong, nContinent = %d, nTaxKind = %d", nContinent, nTaxKind);
 				return;
 			}
 
-			__TAXDETAIL* taxDetail = &taxInfo->mapTaxDetail.find(nTaxKind)->second;
+			__TAXDETAIL* taxDetail = &itTaxDetail->second;
 			taxDetail->nTaxRate = pQuery->GetInt( "nTaxRate" );
 			taxDetail->nTaxCount = pQuery->GetInt( "nTaxCount" );
 			taxDetail->nTaxGold = pQuery->GetInt( "nTaxGold" );