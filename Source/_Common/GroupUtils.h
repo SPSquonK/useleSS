SPSquonK/useleSS

commit 5a05f90c7d9eb39ebdb50c8caa428b11387c41a6

Fix compatibility with VS 2022 v17.6.2
- Disable stl modules
- OH LOOK STD::VIEWS::FILTER IS BROKEN AGAIN BECAUSE ITER_MOVE
HOW WONDERFULL. SO I REPLACE EVERY USE OF STD::VIEWS::FILTER
WITH SOMETHING ELSE BECAUSE WHILE USEFULL, I CAN NOT RELY ON
SOMETHING THAT BREAKS EVERY THREE UPDATES OF MSVC STL.

Source/_Common/GroupUtils.h
@@ -1,21 +1,32 @@
 #pragma once
 
-#include <ranges>
+#include <boost/container/small_vector.hpp>
 
 #include "party.h"
 #include "guild.h"
 
+inline boost::container::small_vector<CUser *, MAX_PTMEMBER_SIZE> AllMembers(const CParty & party) {
+	boost::container::small_vector<CUser *, MAX_PTMEMBER_SIZE> out;
 
+	for (const PartyMember & pm : party.m_aMember) {
+		CUser * pUser = prj.GetUserByID(pm.m_uPlayerId);
+		if (IsValidObj(pUser)) {
+			out.emplace_back(pUser);
+		}
+	}
 
-inline auto AllMembers(const CParty & party) {
-	return std::views::counted(party.m_aMember, party.GetSizeofMember())
-		| std::views::transform([](const PartyMember & pm) { return prj.GetUserByID(pm.m_uPlayerId); })
-		| std::views::filter(IsValidObj);
+	return out;
 }
 
-inline auto AllMembers(const CGuild & guild) {
-	return guild.m_mapPMember
-		| std::views::values
-		| std::views::transform([](const CGuildMember * gm) { return prj.GetUserByID(gm->m_idPlayer); })
-		| std::views::filter(IsValidObj);
+inline boost::container::small_vector<CUser *, 40> AllMembers(const CGuild & guild) {
+	boost::container::small_vector<CUser *, 40> out;
+
+	for (const auto & [_, guildMember] : guild.m_mapPMember) {
+		CUser * pUser = prj.GetUserByID(guildMember->m_idPlayer);
+		if (IsValidObj(pUser)) {
+			out.emplace_back(pUser);
+		}
+	}
+
+	return out;
 }

Source/AccountServer/AccountServer.vcxproj
@@ -66,6 +66,8 @@
       <CompileAs>Default</CompileAs>
       <ConformanceMode>false</ConformanceMode>
       <LanguageStandard>stdcpplatest</LanguageStandard>
+      <BuildStlModules>false</BuildStlModules>
+      <EnableModules>false</EnableModules>
     </ClCompile>
     <Link>
       <AdditionalDependencies>ws2_32.lib;winmm.lib;d3dx9.lib;odbc32.lib;odbccp32.lib;VERSION.LIB;%(AdditionalDependencies)</AdditionalDependencies>

Source/CACHESERVER/CacheServer.vcxproj
@@ -59,6 +59,8 @@
       <AdditionalOptions>/Zc:threadSafeInit- %(AdditionalOptions)</AdditionalOptions>
       <ConformanceMode>false</ConformanceMode>
       <LanguageStandard>stdcpplatest</LanguageStandard>
+      <EnableModules>false</EnableModules>
+      <BuildStlModules>false</BuildStlModules>
     </ClCompile>
     <Link>
       <AdditionalDependencies>odbc32.lib;odbccp32.lib;ws2_32.lib;winmm.lib;VERSION.LIB;psapi.lib;%(AdditionalDependencies)</AdditionalDependencies>

Source/CACHESERVER/Player.cpp
@@ -54,12 +54,12 @@ void CCachePlayerMng::DestroyPlayer(const CDPClient * const pClient) {
 
 	if (pClient) {
 		// ~ Destroy players on WorldServer pClient
-		for (const auto & pPlayer : m_mapPlayers
-			| std::views::values
-			| std::views::filter([pClient](const auto & player) { return player->GetClient() == pClient; })
+		for (const auto & pPlayer : m_mapPlayers | std::views::values
 			) {
-			g_DPCoreClient.SendDestroyPlayer(*pPlayer);
-			g_DPCacheSrvr.DestroyPlayer(pPlayer->GetNetworkId());
+			if (pPlayer->GetClient() == pClient) {
+				g_DPCoreClient.SendDestroyPlayer(*pPlayer);
+				g_DPCacheSrvr.DestroyPlayer(pPlayer->GetNetworkId());
+			}
 		}
 	} else {
 		// ~ DestroyAllPlayers (= Core is dead)

Source/CERTIFIER/Certifier.vcxproj
@@ -59,6 +59,8 @@
       <AdditionalOptions>/Zc:threadSafeInit- %(AdditionalOptions)</AdditionalOptions>
       <ConformanceMode>false</ConformanceMode>
       <LanguageStandard>stdcpplatest</LanguageStandard>
+      <EnableModules>false</EnableModules>
+      <BuildStlModules>false</BuildStlModules>
     </ClCompile>
     <Link>
       <AdditionalDependencies>odbc32.lib;odbccp32.lib;ws2_32.lib;d3dx9.lib;winmm.lib;%(AdditionalDependencies)</AdditionalDependencies>

Source/CORESERVER/CoreServer.vcxproj
@@ -58,6 +58,8 @@
       <CompileAs>Default</CompileAs>
       <AdditionalOptions>/Zc:threadSafeInit- %(AdditionalOptions)</AdditionalOptions>
       <LanguageStandard>stdcpplatest</LanguageStandard>
+      <EnableModules>false</EnableModules>
+      <BuildStlModules>false</BuildStlModules>
     </ClCompile>
     <Link>
       <AdditionalDependencies>odbc32.lib;odbccp32.lib;ws2_32.lib;winmm.lib;pdh.lib;VERSION.LIB;%(AdditionalDependencies)</AdditionalDependencies>

Source/LOGINSERVER/LoginServer.vcxproj
@@ -59,6 +59,8 @@
       <AdditionalOptions>/Zc:threadSafeInit- %(AdditionalOptions)</AdditionalOptions>
       <ConformanceMode>false</ConformanceMode>
       <LanguageStandard>stdcpplatest</LanguageStandard>
+      <EnableModules>false</EnableModules>
+      <BuildStlModules>false</BuildStlModules>
     </ClCompile>
     <Link>
       <AdditionalDependencies>ws2_32.lib;winmm.lib;%(AdditionalDependencies)</AdditionalDependencies>

Source/Neuz/Neuz.vcxproj
@@ -57,6 +57,8 @@
       <AdditionalOptions>/Zc:threadSafeInit- %(AdditionalOptions)</AdditionalOptions>
       <ConformanceMode>false</ConformanceMode>
       <LanguageStandard>stdcpplatest</LanguageStandard>
+      <EnableModules>false</EnableModules>
+      <BuildStlModules>false</BuildStlModules>
     </ClCompile>
     <Link>
       <AdditionalDependencies>d3dx9.lib;d3d9.lib;d3dxof.lib;dxerr9.lib;dsound.lib;winmm.lib;dxguid.lib;imm32.lib;ws2_32.lib;VERSION.LIB;legacy_stdio_definitions.lib;%(AdditionalDependencies)</AdditionalDependencies>

commit 445ee3dc2cc115319e6ec9d8460b02435ec52c7a

Fix all members of party look up
- The bug was introduced in 5a05f90c7d9eb39ebdb50c8caa428b11387c41a6
- Thanks Kotori and his player base for debugging my future server

Source/_Common/GroupUtils.h
@@ -8,7 +8,7 @@
 inline boost::container::small_vector<CUser *, MAX_PTMEMBER_SIZE> AllMembers(const CParty & party) {
 	boost::container::small_vector<CUser *, MAX_PTMEMBER_SIZE> out;
 
-	for (const PartyMember & pm : party.m_aMember) {
+	for (const PartyMember & pm : std::span(party.m_aMember, party.GetSizeofMember())) {
 		CUser * pUser = prj.GetUserByID(pm.m_uPlayerId);
 		if (IsValidObj(pUser)) {
 			out.emplace_back(pUser);