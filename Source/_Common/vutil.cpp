SPSquonK/useleSS

commit 7f8ca3ae5e39d52951251a512d973ba03a5679c6

ComputeShortenName function
Unit test: https://gist.github.com/SPSquonK/8f57d714389c80f9b73b32dfca3650cf

Source/_Common/vutil.cpp
@@ -15,6 +15,31 @@ int GetCharLen( const CHAR* pStr )
 	return (int) (pNext - pStr );
 }
 
+void ComputeShortenName(CHAR * pDest, const CHAR * pSrc, size_t nCount) {
+	size_t nOffset = 0;
+
+	while (nCount > 0) {
+		if (pSrc[nOffset] == '\0') break;
+
+		const int nLen = GetCharLen(&pSrc[nOffset]);
+		nOffset += nLen;
+		--nCount;
+	}
+
+	std::memcpy(pDest, pSrc, nOffset);
+	pDest[nOffset] = '\0';
+
+	if (pSrc[nOffset] == '\0') {
+		// No more characters
+	} else if (pSrc[nOffset + GetCharLen(&pSrc[nOffset])] == '\0') {
+		// Only one character: just write it instead of 3 dots
+		std::strcat(pDest, &pSrc[nOffset]);
+	} else {
+		// Add ...
+		std::strcat(pDest, "...");
+	}
+}
+
 // 외국어포함 문장의 길이를 잘라준다.
 int GetStrCut( const CHAR* pSrc, CHAR* pDest, int nCount )
 {
@@ -43,7 +68,6 @@ int GetStrLen( const CHAR* pSrc )
 	
 	while( pSrc[ nOffset ] != NULL )
 	{
-		nLen = GetCharLen( &pSrc[ nOffset ]  );
 		nOffset += nLen;
 		nCount++;
 	}

Source/CORESERVER/DPCacheSrvr.cpp
@@ -1486,7 +1486,6 @@ void CDPCacheSrvr::OnGuildNickName( CAr & ar, DPID dpidCache, DPID dpidUser, u_l
 
 	if( pGuild->IsMaster( pMaster->uKey ) )
 	{
-//		int nLength = GetStrLen( strNickName );
 		int nLen	= lstrlen( strNickName );
 
 		if( nLen < 2 || nLen > 12 )

Source/Neuz/DPClient.cpp
@@ -5963,14 +5963,7 @@ void CDPClient::OnGCLog( CAr & ar )
 
 	static constexpr auto MakeName = [](const char * name) -> StaticString<MAX_NAME> {
 		StaticString<MAX_NAME> retval;
-		GetStrCut(name, retval.GetBuffer(), 10);
-
-		if (10 <= GetStrLen(name)) {
-			strcat(szBuf, "...");
-		} else {
-			retval = name;
-		}
-
+		ComputeShortenName(retval.GetBuffer(), name, 10);
 		return retval;
 	};
 

Source/_Common/vutil.h
@@ -26,8 +26,11 @@ BOOL IsHangul( WORD word );
 BOOL IsNative( LPCTSTR lpszStr );
 
 // 모든언어의 문자관련
+[[deprecated("Use ComputeShortenName instead of GetStrCut + GetStrLen")]]
 int GetStrCut( const CHAR* pSrc, CHAR* pDest, int nCount );
-int GetStrLen( const CHAR* pSrc );
+int GetStrLen(const CHAR * pSrc);
+
+void ComputeShortenName(CHAR * pDest, const CHAR * pSrc, size_t nCount);
 
 
 #define IMGBIT_8   0  

Source/_Interface/WndField.cpp
@@ -6700,16 +6700,7 @@ void CWndGuildWarState::InsertGuild( const char szGuild[], const char szName[], 
 	if( nNum == 0 )
 		return;
 
-	GetStrCut( szGuild, m_aList[m_nMax].szGuild, 8 );
-
-	if( GetStrLen( szGuild ) >= 8 )
-	{
-		_tcscat( m_aList[m_nMax].szGuild, "..." );
-	}
-	else
-	{
-		_tcscpy( m_aList[m_nMax].szGuild, szGuild );
-	}
+	ComputeShortenName(m_aList[m_nMax].szGuild, szGuild, 8);
 
 	_tcscpy( m_aList[m_nMax].szName, szName );
 	m_aList[m_nMax].nNum = nNum;
@@ -6898,16 +6889,7 @@ void CWndGuildCombatRanking::InsertGuild( const char szGuild[], int nWinCount )
 
 	GUILDNAME p;
 	memset( &p, 0, sizeof(GUILDNAME) );
-	GetStrCut( szGuild, p.szGuild, 8 );
-	
-	if( GetStrLen( szGuild ) >= 8 )
-	{
-		_tcscat( p.szGuild, "..." );
-	}
-	else
-	{
-		_tcscpy( p.szGuild, szGuild );
-	}
+	ComputeShortenName(p.szGuild, szGuild, 8);
 	
 	m_multimapRanking.emplace(nWinCount, p);
 

Source/_Interface/WndPost.cpp
@@ -598,11 +598,7 @@ void CWndPostRead::SetValue( int nMailIndex )
 	else
 		lstrcpy( lpszPlayer, lpszPlayerString );
 
-	memset( szCutTitle, 0, sizeof(szCutTitle) );
-	GetStrCut( lpszPlayer, szCutTitle, 16 );
-	
-	if( GetStrLen( lpszPlayer ) > 16 )
-		_tcscat( szCutTitle, "..." );
+	ComputeShortenName(szCutTitle, lpszPlayer, 17);
 
 	CWndEdit * pWndEdit1 = (CWndEdit *)GetDlgItem(WIDC_EDIT1);
 	if( lstrlen( lpszPlayer ) > 0 )
@@ -610,26 +606,13 @@ void CWndPostRead::SetValue( int nMailIndex )
 	else
 		pWndEdit1->SetString("Unknown");
 	
-	memset( szCutTitle, 0, sizeof(szCutTitle) );
-	GetStrCut( pMail->m_szTitle, szCutTitle, 13 );
-	
-	if( GetStrLen( pMail->m_szTitle ) >= 13 )
-	{
-		_tcscat( szCutTitle, "..." );
-	}
+	ComputeShortenName(szCutTitle, pMail->m_szTitle, 13);
 
 	CWndEdit * pWndEdit2 = (CWndEdit *)GetDlgItem(WIDC_EDIT2);
 	CWndText * pWndText = (CWndText *)GetDlgItem(WIDC_TEXT1);
-	if( lstrlen( lpszPlayer ) > 0 )
-	{
-		pWndEdit2->SetString(szCutTitle);
-		pWndText->SetString(pMail->m_szText);
-	}
-	else
-	{
-		pWndEdit2->SetString( "" );
-		pWndText->SetString( "" );
-	}
+	pWndEdit2->SetString(szCutTitle);
+	pWndText->SetString(pMail->m_szText);
+
 
 	g_DPlay.SendQueryReadMail( pMail->m_nMail );
 
@@ -1009,11 +992,7 @@ void CWndPostReceive::OnDraw( C2DRender* p2DRender )
 		else
 			lstrcpy( lpszPlayer, lpszPlayerString );
 		
-		memset( szCutTitle, 0, sizeof(szCutTitle) );
-		GetStrCut( lpszPlayer, szCutTitle, 16 );
-		
-		if( GetStrLen( lpszPlayer ) > 16 )
-			_tcscat( szCutTitle, "..." );
+		ComputeShortenName(szCutTitle, lpszPlayer, 17);
 
 		if( lstrlen( lpszPlayer ) > 0 )
 		{
@@ -1045,16 +1024,9 @@ void CWndPostReceive::OnDraw( C2DRender* p2DRender )
 			p2DRender->TextOut( sx + 190,  sy+3, szDay,  D3DCOLOR_XRGB( (int)v2Result.x, (int)v2Result.y, 0 ) );			
 		else
 			p2DRender->TextOut( sx + 193,  sy+3, szDay,  D3DCOLOR_XRGB( (int)v2Result.x, (int)v2Result.y, 0 ) );			
+
+		ComputeShortenName(szCutTitle, mailbox[i]->m_szTitle, 13);
 		
-		memset( szCutTitle, 0, sizeof(szCutTitle) );
-		GetStrCut( mailbox[i]->m_szTitle, szCutTitle, 13 );
-		
-		if( GetStrLen( mailbox[i]->m_szTitle ) >= 13 )
-		{
-			_tcscat( szCutTitle, "..." );
-		}
-		
-//		if( GetFocusWnd() == this )
 		{
 			CRect  rect;
 			rect.SetRect( sx+4, sy - 4, sx+240, sy + 36 ); 	
@@ -1069,10 +1041,7 @@ void CWndPostReceive::OnDraw( C2DRender* p2DRender )
 			}		
 		}
 		
-		if( lstrlen( lpszPlayer ) > 0 )
-			p2DRender->TextOut( sx + 60,  sy+18, szCutTitle,  D3DCOLOR_XRGB( 0, 0, 190 ) );
-		else
-			p2DRender->TextOut( sx + 60,  sy+18, "",  D3DCOLOR_XRGB( 0, 0, 190 ) );
+		p2DRender->TextOut( sx + 60,  sy+18, szCutTitle,  D3DCOLOR_XRGB( 0, 0, 190 ) );
 
 		sy += 40;
 	}	

Source/_Interface/WndWorld.cpp
@@ -1126,7 +1126,6 @@ BOOL CWndWorld::OnEraseBkgnd(C2DRender* p2DRender)
 	}
 	
 	{
-		char szMsgGuild[128] = { 0 };			
 		char szMsg[128] = {0,};
 		int nX = 20;
 		int nY = (m_rectWindow.Height()/2) - 80;
@@ -1162,20 +1161,9 @@ BOOL CWndWorld::OnEraseBkgnd(C2DRender* p2DRender)
 
 				nY += (cSize.cy+10);
 
-				char szBuf[MAX_NAME] = {0,};
-
-				GetStrCut( GuildCombatJoin.szJoinGuildName, szBuf, 8 );
+				char szMsgGuild[MAX_NAME] = {0,};
+				ComputeShortenName(szMsgGuild, GuildCombatJoin.szJoinGuildName, 8);
 				
-				if( 8 <= GetStrLen(GuildCombatJoin.szJoinGuildName) )
-				{
-					strcat( szBuf, "..." );
-				}
-				else
-				{
-					strcpy( szBuf, GuildCombatJoin.szJoinGuildName );
-				}
-				
-				sprintf( szMsgGuild, "%s", szBuf );
 				sprintf( szMsg, "%.2d/%.2d", GuildCombatJoin.nJoinSize, GuildCombatJoin.nJoinSize + GuildCombatJoin.nOutSize );
 
 				crect = CRect(nX-10, nY-5, nX+160, nY+18);
@@ -9317,13 +9305,7 @@ void GuildCombatInfo::RenderMyGuildStatus(C2DRender * p2DRender) {
 
 		LPCTSTR str	= CPlayerDataCenter::GetInstance()->GetPlayerString( GuildRate.m_uidPlayer );
 		
-		if (5 <= GetStrLen(str)) {
-			memset(szBuf, 0, sizeof(szBuf));
-			GetStrCut(str, szBuf, 5);
-			strcat(szBuf, "...");
-		} else {
-			strcpy(szBuf, str);
-		}		
+		ComputeShortenName(szBuf, str, 5);
 
 		DWORD dwFontColor;
 		if (GuildRate.m_uidPlayer == g_pPlayer->m_idPlayer) {
@@ -9452,16 +9434,7 @@ void GuildCombatPrecedence::Render(C2DRender * p2DRender, const CRect clientRect
 			memset( szBuf, 0, sizeof(CHAR)*MAX_NAME );
 
 			LPCSTR str = GetGuildName(guildId);
-			GetStrCut( str, szBuf, 5 );
-				
-			if( 5 <= GetStrLen(str) )
-			{
-				strcat( szBuf, "..." );
-			}
-			else
-			{
-				strcpy( szBuf, str );
-			}			
+			ComputeShortenName(szBuf, str, 5);
 
 			if( nOldPoint != nPoint )
 			{
@@ -9538,18 +9511,7 @@ void GuildCombatPrecedence::Render(C2DRender * p2DRender, const CRect clientRect
 
 		{
 			LPCTSTR str	= CPlayerDataCenter::GetInstance()->GetPlayerString( uiPlayer );
-			memset( szBuf, 0, sizeof(CHAR)*MAX_NAME );
-				
-			GetStrCut( str, szBuf, 5 );
-				
-			if( 5 <= GetStrLen(str) )
-			{
-				strcat( szBuf, "..." );
-			}
-			else
-			{
-				strcpy( szBuf, str );
-			}			
+			ComputeShortenName(szBuf, str, 5);
 
 			if( uiPlayer == g_pPlayer->m_idPlayer )
 			{
@@ -9586,18 +9548,7 @@ void GuildCombatPrecedence::Render(C2DRender * p2DRender, const CRect clientRect
 		cPoint.y += nGap;
 
 		LPCTSTR str = g_pPlayer->GetName();
-		memset( szBuf, 0, sizeof(CHAR)*MAX_NAME );
-			
-		GetStrCut( str, szBuf, 5 );
-			
-		if( 5 <= GetStrLen(str) )
-		{
-			strcat( szBuf, "..." );
-		}
-		else
-		{
-			strcpy( szBuf, str );
-		}						
+		ComputeShortenName(szBuf, str, 5);
 
 		dwFontColor = 0xFF9ED3FF;
 		strFormat.Format( "%2d", nPlayerRate );

commit 04ad3be73a4701087013aeabe69fa71501ccc372

Fix 2 mistakes in GetStrCut and GetStrlLen
- GetStrLen: Wrong nLen line deleted introduced in
7f8ca3ae5e39d52951251a512d973ba03a5679c6
- GetStrCut: Terminal zero not placed at the right place introduced
in abaa9390ea6f7ba730653e2ec23a2bb2b32387d7

Thanks Kotori for noticing these two mistakes

Source/_Common/vutil.cpp
@@ -41,22 +41,18 @@ void ComputeShortenName(CHAR * pDest, const CHAR * pSrc, size_t nCount) {
 }
 
 // 외국어포함 문장의 길이를 잘라준다.
-int GetStrCut( const CHAR* pSrc, CHAR* pDest, int nCount )
-{
+int GetStrCut( const CHAR* pSrc, CHAR* pDest, int nCount ) {
 	int nOffset = 0;
-	int nLen;
 	
-	while( nCount )
-	{
-		nLen = GetCharLen( &pSrc[ nOffset ]  );
+	while (nCount) {
+		const int nLen = GetCharLen(&pSrc[nOffset]);
 		nOffset += nLen;
 		nCount--;
 	}
 	
 	memcpy( pDest, pSrc, sizeof(char)*nOffset);
-	pDest[nOffset + 1] = '\0';
-	
-	return 1;
+	pDest[nOffset] = '\0';
+	return nOffset;
 }	
 
 // 외국어포함 문자 갯수를 알아냄
@@ -64,10 +60,10 @@ int GetStrLen( const CHAR* pSrc )
 {
 	int nCount = 0;
 	int nOffset = 0;
-	int nLen;
 	
-	while( pSrc[ nOffset ] != NULL )
+	while( pSrc[ nOffset ] != '\0')
 	{
+		const int nLen = GetCharLen(&pSrc[nOffset]);
 		nOffset += nLen;
 		nCount++;
 	}