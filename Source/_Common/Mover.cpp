SPSquonK/useleSS

commit 78fc6f27522eb527b14b6bcd0e7bbfabb1970b96

Dying outside of the guildwar map doesn't count towards a guild war kill anymore
- Apparently, this bug was known by everybody but me, Kotori, and
the player that triggered this bug today ... according to
Kotori's player base.
- I never mashed my X key so hard

- Thanks Kotori for reporting the bug and the fix idea

- Hey you. Yeah you. YOU can also get cited in every other commit,
Just send me bug fixes ideas, PRs or stuff like that

Source/_Common/Mover.cpp
@@ -4321,7 +4321,9 @@ int CMover::DoDie( CCtrl *pAttackCtrl, DWORD dwMsg )
 			CUser * thisPlayer = static_cast<CUser *>(this);
 			CUser * attackerPlayer = static_cast<CUser *>(pAttacker);
 
-			GetWorld()->OnDie(thisPlayer, attackerPlayer);
+			if (GetWorld()->GetID() == WI_WORLD_GUILDWAR) {
+				GetWorld()->OnDie(thisPlayer, attackerPlayer);
+			}
 		}
 	}
 

commit 6a617b06faf5bfa9d0e8dea0cdf06d20c5d006ad

Fix 1vs1 Guild Combat killing
Fixes the bug introduced in 78fc6f27522eb527b14b6bcd0e7bbfabb1970b96

Source/_Common/Mover.cpp
@@ -4321,9 +4321,7 @@ int CMover::DoDie( CCtrl *pAttackCtrl, DWORD dwMsg )
 			CUser * thisPlayer = static_cast<CUser *>(this);
 			CUser * attackerPlayer = static_cast<CUser *>(pAttacker);
 
-			if (GetWorld()->GetID() == WI_WORLD_GUILDWAR) {
-				GetWorld()->OnDie(thisPlayer, attackerPlayer);
-			}
+			GetWorld()->OnDie(thisPlayer, attackerPlayer);
 		}
 	}
 

Source/_Common/World.cpp
@@ -1747,15 +1747,22 @@ void CWorld::OnDie(CUser * pDie, CUser * pAttacker ) {
 }
 
 void CWorld::_OnDie() {
-	for (const auto & onDie : m_OnDie) {
-		g_GuildCombatMng.OutWar(onDie.pDie, NULL);
-		g_GuildCombatMng.GetPoint(onDie.pAttacker, onDie.pDie);
-		
-		const int nIndex = g_GuildCombat1to1Mng.GetTenderGuildIndexByUser(onDie.pDie);
-		if (nIndex != NULL_ID) {
+	if (GetID() == WI_WORLD_GUILDWAR) {
+		for (const auto & onDie : m_OnDie) {
+			g_GuildCombatMng.OutWar(onDie.pDie, NULL);
+			g_GuildCombatMng.GetPoint(onDie.pAttacker, onDie.pDie);
+		}
+	}
+
+	if (GetID() >= WI_WORLD_GUILDWAR1TO1_0 && GetID() <= WI_WORLD_GUILDWAR1TO1_L) {
+		for (const auto & onDie : m_OnDie) {
+			const int nIndex = g_GuildCombat1to1Mng.GetTenderGuildIndexByUser(onDie.pDie);
+			if (nIndex == NULL_ID) continue;
+			
 			const int nStageId = g_GuildCombat1to1Mng.m_vecTenderGuild[nIndex].nStageId;
-			if (nStageId != NULL_ID)
-				g_GuildCombat1to1Mng.m_vecGuilCombat1to1[nStageId].SetLost(onDie.pDie);
+			if (nStageId == NULL_ID) continue;
+			
+			g_GuildCombat1to1Mng.m_vecGuilCombat1to1[nStageId].SetLost(onDie.pDie);
 		}
 	}
 