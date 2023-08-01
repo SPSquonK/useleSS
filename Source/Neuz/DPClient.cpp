SPSquonK/useleSS

commit f6dba4936c91749589cd4991ec49bde4aac38dfa

CModelObject::GetForcePos

Source/Neuz/DPClient.cpp
@@ -719,7 +719,7 @@ void CDPClient::OnSnapshot( CAr & ar )
 
 		const u_long endedAt = ar.GetOffset();
 
-		if (ar.GoToOffset(startedAt + packetSize) == CAr::GoToOffsetAnswer::TooFar) {
+		if (ar.GoToOffset(startedAt + packetSize) != CAr::GoToOffsetAnswer::SamePlace) {
 			if (!glitchedSnapshotsId.contains(hdr)) {
 				Error("Snapshot %x went too far", hdr);
 				glitchedSnapshotsId.emplace(hdr);

Source/_Common/ModelObject.cpp
@@ -2068,93 +2068,28 @@ void	CModelObject::SetMotionBlending( BOOL bFlag )
 }
 
 
-void	CModelObject::GetForcePos( D3DXVECTOR3 *vOut, int nIdx, int nParts, const D3DXMATRIX &mWorld )
-{
-	D3DXMATRIX *pmLocal;
-	O3D_ELEMENT		*pElem = GetParts( nParts );		// 오른손 무기의 포인터
+std::optional<D3DXVECTOR3> CModelObject::GetForcePos(int nParts, const D3DXMATRIX & mWorld) {
+	O3D_ELEMENT * pElem = GetParts(nParts);		// 오른손 무기의 포인터
+
+	if (!pElem) return std::nullopt;
+	if (pElem->m_pObject3D == NULL)		return std::nullopt;
+	if (pElem->m_pObject3D->m_vForce1.x == 0)	return std::nullopt;
+	if (pElem->m_pObject3D->m_vForce2.x == 0)	return std::nullopt;
+
 	D3DXMATRIX	m1;
+	if (pElem->m_nParentIdx == GetRHandIdx()) {
+		m1 = pElem->m_mLocalRH * m_mUpdateBone[pElem->m_nParentIdx];
+	} else if (pElem->m_nParentIdx == GetLHandIdx()) {
+		m1 = pElem->m_mLocalLH * m_mUpdateBone[pElem->m_nParentIdx];
+	} else {
+		m1 = m_mUpdateBone[pElem->m_nParentIdx];						// 일단 에러는 안나게 이렇게 한다.
+	}
+
 	D3DXVECTOR3		v1;
-
-	if( !pElem )
-		return;
-	
-	if( pElem->m_pObject3D == NULL )		return;
-	
-	if( pElem->m_pObject3D->m_vForce1.x == 0 )	return;
-	if( pElem->m_pObject3D->m_vForce2.x == 0 )	return;
-	
-	if( pElem->m_nParentIdx == GetRHandIdx() )
-		pmLocal = &pElem->m_mLocalRH;
-	else if( pElem->m_nParentIdx == GetLHandIdx() )
-		pmLocal = &pElem->m_mLocalLH;
-	else
-	{
-		D3DXMatrixIdentity( &m1 );
-		pmLocal = &m1;						// 일단 에러는 안나게 이렇게 한다.
-	}
-		
-	
-//	if( m_pMotion )
-//		m_pMotion->AnimateBone( m_mUpdateBone, m_pMotionOld, m_fFrameCurrent, GetNextFrame(), m_nFrameOld, m_bMotionBlending, m_fBlendWeight );		// 일단 뼈대가 있다면 뼈대 애니메이션 시킴
-	
-	D3DXMatrixMultiply( &m1, pmLocal, &m_mUpdateBone[ pElem->m_nParentIdx ] );
-	if( nIdx == 0 )
-		D3DXVec3TransformCoord( &v1, &(pElem->m_pObject3D->m_vForce1), &m1 );
-	else
-		D3DXVec3TransformCoord( &v1, &(pElem->m_pObject3D->m_vForce2), &m1 );
-	
-	D3DXVec3TransformCoord( &v1, &v1, &mWorld );
-	*vOut = v1;
-	
+	D3DXVec3TransformCoord(&v1, &(pElem->m_pObject3D->m_vForce1), &m1);
+	D3DXVec3TransformCoord(&v1, &v1, &mWorld);
+	return v1;
 }
-
-
-void	CModelObject::GetForcePos( D3DXVECTOR3 *vOut, int nIdx, int nParts, const D3DXMATRIX &mWorld, float fScale)
-{
-	D3DXMATRIX mLocal;
-	O3D_ELEMENT		*pElem = GetParts( nParts );		// 오른손 무기의 포인터
-	D3DXMATRIX	m1;
-	D3DXVECTOR3		v1;
-
-	if( !pElem )
-		return;
-	
-	if( pElem->m_pObject3D == NULL )		return;
-	
-	if( pElem->m_pObject3D->m_vForce1.x == 0 )	return;
-	if( pElem->m_pObject3D->m_vForce2.x == 0 )	return;
-	
-	if( pElem->m_nParentIdx == GetRHandIdx() )
-	{
-		mLocal = pElem->m_mLocalRH;
-		D3DXMatrixScaling(&mLocal, fScale, fScale, fScale);
-	}
-	else if( pElem->m_nParentIdx == GetLHandIdx() )
-	{
-		mLocal = pElem->m_mLocalLH;
-		D3DXMatrixScaling(&mLocal, fScale, fScale, fScale);
-	}
-	else
-	{
-		D3DXMatrixIdentity( &m1 );
-		mLocal = m1;						// 일단 에러는 안나게 이렇게 한다.
-	}
-		
-	
-//	if( m_pMotion )
-//		m_pMotion->AnimateBone( m_mUpdateBone, m_pMotionOld, m_fFrameCurrent, GetNextFrame(), m_nFrameOld, m_bMotionBlending, m_fBlendWeight );		// 일단 뼈대가 있다면 뼈대 애니메이션 시킴
-	
-	D3DXMatrixMultiply( &m1, &mLocal, &m_mUpdateBone[ pElem->m_nParentIdx ] );
-	if( nIdx == 0 )
-		D3DXVec3TransformCoord( &v1, &(pElem->m_pObject3D->m_vForce1), &m1 );
-	else
-		D3DXVec3TransformCoord( &v1, &(pElem->m_pObject3D->m_vForce2), &m1 );
-	
-	D3DXVec3TransformCoord( &v1, &v1, &mWorld );
-	*vOut = v1;
-	
-}
-
 
 // Calcualtes the position of the center of the fist.
 D3DXVECTOR3 CModelObject::GetHandPos(int nParts, const D3DXMATRIX & mWorld) {

Source/_Common/ModelObject.h
@@ -199,8 +199,8 @@ public:
 		return NULL; 
 	}		
 	void	SetExtTexture( LPDIRECT3DTEXTURE9 pTexture ) { m_pCloakTexture = pTexture; }
-	void	GetForcePos( D3DXVECTOR3 *vOut, int nIdx, int nParts, const D3DXMATRIX &mWorld );
-	void	GetForcePos( D3DXVECTOR3 *vOut, int nIdx, int nParts, const D3DXMATRIX &mWorld, float fScale);
+	[[nodiscard]] std::optional<D3DXVECTOR3> GetForcePos(int nParts, const D3DXMATRIX & mWorld);
+
 	[[nodiscard]] D3DXVECTOR3	GetHandPos(int nParts, const D3DXMATRIX & mWorld);
 		
 	int		MovePart( int nDstPart, int nSrcPart );

Source/_Common/Mover.cpp
@@ -8632,14 +8632,14 @@ void	CMover::ProcessCollecting( void )
 			const ItemProp *pHandProp	= GetActiveHandItemProp();
 			if( pHandProp->dwSfxObj2 != NULL_ID )
 			{
-				D3DXVECTOR3 vSrc, vLocal	= D3DXVECTOR3( 0, 0.5f, 0 );
-				( (CModelObject *)m_pModel )->GetForcePos( &vLocal, 0, PARTS_RWEAPON, GetMatrixWorld() );
-				vSrc	= vLocal;
+				D3DXVECTOR3 vSrc = ((CModelObject *)m_pModel)
+					->GetForcePos(PARTS_RWEAPON, GetMatrixWorld())
+					.value_or(D3DXVECTOR3(0, 0.5f, 0));
+
 				CSfx *pSfx	= CreateSfx( pHandProp->dwSfxObj2, vSrc, GetId(), D3DXVECTOR3( 0, 0, 0 ), NULL_ID, -1 );
 				if( pSfx )
 				{
 					pSfx->SetAngle( -GetAngle() + 90.0f );
-					//pSfx->SetAngleX(90.0f);
 					m_dwFlag	|= MVRF_COLLECT;
 				}
 			}

Source/_Common/MoverActEvent.cpp
@@ -2028,9 +2028,10 @@ int		CMover::OnActCollecting()
 		ItemProp *pHandProp = GetActiveHandItemProp();
 		if( pHandProp->dwSfxObj2 != NULL_ID )
 		{
-			D3DXVECTOR3 vSrc, vLocal = D3DXVECTOR3(0, 0.5f, 0);
-			((CModelObject *)m_pModel)->GetForcePos( &vLocal, 0, PARTS_RWEAPON, GetMatrixWorld() );
-			vSrc = vLocal;
+			D3DXVECTOR3 vSrc = ((CModelObject *)m_pModel)
+				->GetForcePos(PARTS_RWEAPON, GetMatrixWorld())
+				.value_or(D3DXVECTOR3(0, 0.5f, 0));
+
 			CSfx *pSfx = CreateSfx( pHandProp->dwSfxObj2, vSrc, GetId(), D3DXVECTOR3(0,0,0), NULL_ID, -1 );
 			if( pSfx )
 			{

Source/_Common/Sfx.cpp
@@ -2632,10 +2632,10 @@ void CSfxCollect::Process()
 		return;
 	}
 	
-	D3DXVECTOR3 vPos;
-	((CModelObject *)pMover->m_pModel)->GetForcePos( &vPos, 0, PARTS_RWEAPON, pMover->GetMatrixWorld() );
-	
-	SetPos( vPos );
+	const auto vPos = ((CModelObject *)pMover->m_pModel)->GetForcePos(PARTS_RWEAPON, pMover->GetMatrixWorld());
+	if (vPos) {
+		SetPos(*vPos);
+	}
 
 	if( m_pSfxObj->Process() )
 		m_pSfxObj->m_nCurFrame = 0;

commit 494ef8f468bee0f4d850b11ec91d55d3fe3b6f4f

Fix bugs in CDPClient::OnSnapshot
> Set back Error printing of CDPClient::OnSnapshot to TooFar
- It was never intended to be commited to TooFar. It was just
for testing purpose
- Thanks Kotori for reporting this error introduced in
f6dba4936c91749589cd4991ec49bde4aac38dfa

> Make the set glitchedSnapshotsId a global variable as it was
originally intended

Source/Neuz/DPClient.cpp
@@ -288,18 +288,12 @@ BOOL CDPClient::Connect( LPSTR lpszAddr, USHORT uPort )
 	return ( m_fConn = FALSE );
 }
 
-BYTE g_hdr, g_Prev;
-
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 // Receiver
 void CDPClient::OnSnapshot( CAr & ar )
 {
-	std::set<WORD> glitchedSnapshotsId;
-
 	OBJID objidPlayer;
 	short cb;
-//	BYTE hdr, prev	= 0x00;
-	WORD	prev	= 0x0000;
 
 	ar >> objidPlayer >> cb;
 	while( cb-- )
@@ -311,7 +305,6 @@ void CDPClient::OnSnapshot( CAr & ar )
 		m_traficLog.Add( (BYTE)( hdr ) );
 #endif	// __TRAFIC_1218
 //		TRACE( "hdr = %04x, cb = %d\n", hdr, cb );
-		g_hdr = (BYTE)( hdr );
 
 		switch( hdr )
 		{
@@ -714,14 +707,11 @@ void CDPClient::OnSnapshot( CAr & ar )
 					break;
 				}
 		}
-		prev	= hdr;
-		g_Prev	= (BYTE)( prev );
 
-		const u_long endedAt = ar.GetOffset();
-
-		if (ar.GoToOffset(startedAt + packetSize) != CAr::GoToOffsetAnswer::SamePlace) {
+		if (ar.GoToOffset(startedAt + packetSize) == CAr::GoToOffsetAnswer::TooFar) {
+			static std::set<WORD> glitchedSnapshotsId;
 			if (!glitchedSnapshotsId.contains(hdr)) {
-				Error("Snapshot %x went too far", hdr);
+				Error("Snapshot 0x%04x went too far", hdr);
 				glitchedSnapshotsId.emplace(hdr);
 			}
 		}