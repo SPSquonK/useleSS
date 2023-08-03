7730f971baf7d75ea7c9444db86a36260a62e4f1
SquonK <sp@sqk.fr>
2023-08-03 17:14:50 +0200

    Clean Rangda code
    In this code refactor, the CRangda fields are split in two:
    the props that are const and the state that is not const.

--- Source/WORLDSERVER/rangda.cpp
+++ Source/WORLDSERVER/rangda.cpp
@@ -4,135 +4,114 @@
 #include "worldmng.h"
 #include "lord.h"
 
-CRangda::CRangda( DWORD dwMonster )
-:
-m_dwMonster( dwMonster ),
-m_nInterval( 0 ),
-m_nReplace( 0 ),
-m_bActiveAttack( FALSE ),
-m_nGenerateCountdown( 0 ),
-m_nReplaceCountdown( 0 ),
-m_bReplaceable( TRUE ),
-m_nOldPos( -1 ),
-m_objid( NULL_ID )
-{
+////////////////////////////////////////////////////////////////////////////////
+
+bool CRangda::IsDamaged(CMover * pMonster) {
+	return (pMonster->GetHitPoint() < pMonster->GetMaxHitPoint());
 }
 
-void CRangda::AddPos( const RANGDA_POS & vPos )
-{	// 좌표 추가
-	m_vvPos.push_back( vPos );
+
+////////////////////////////////////////////////////////////////////////////////
+
+CRangda::CRangda(Prop prop)
+	: prop(prop), state() {}
+
+void CRangda::OnTimer() {
+	CMover * pMonster = GetMonster();
+	if (IsValidObj(pMonster)) {
+		ProcessReplace(pMonster);
+	} else {
+		ProcessGenerate();
+	}
 }
 
-CRangda::RANGDA_POS CRangda::GetRandomPos( void )
-{	// 추가된 좌표 중 하나를 임의로 반환
-	ASSERT( !m_vvPos.empty() );
-	
-	// 추가된 좌표가 하나라면 아래의 규칙을
-	// 따를 수 없으므로 그대로 반환
-	if( m_vvPos.size() == 1 )
+////////
+
+void CRangda::SetObj(OBJID objid) {
+	state.m_objid = objid;
+	state.m_nGenerateCountdown = prop.m_nInterval;
+	state.m_nReplaceCountdown = prop.m_nReplace;
+	state.m_bReplaceable = true;
+}
+
+void CRangda::CreateMonster() {
+	const RANGDA_POS pos = prop.GetRandomPos(state.m_nOldPos);
+	CWorld * const pWorld = g_WorldMng.GetWorld(pos.dwWorldId);
+	if (!pWorld) return;
+
+	CMover * const pMonster = static_cast<CMover *>(::CreateObj(OT_MOVER, prop.m_dwMonster));
+	if (!pMonster) return;
+
+	pMonster->m_bActiveAttack = prop.m_bActiveAttack;
+	pMonster->SetPos(pos.vPos);
+	pMonster->InitMotion(MTI_STAND);
+	pMonster->UpdateLocalMatrix();
+	pMonster->AddItToGlobalId();
+	pWorld->ADDOBJ(pMonster, FALSE, nDefaultLayer);
+	SetObj(pMonster->GetId());
+}
+
+CMover * CRangda::GetMonster() {
+	if (state.m_objid == NULL_ID) {
+		return nullptr;
+	}
+
+	CMover * pMonster = prj.GetMover(state.m_objid);
+	if (!pMonster) {
+		state.m_objid = NULL_ID;
+		return nullptr;
+	}
+
+	return pMonster;
+}
+
+////////
+
+void CRangda::ProcessReplace(CMover * pMonster) {
+	if (!state.m_bReplaceable) return;
+
+	if (IsDamaged(pMonster)) {
+		state.m_bReplaceable = false;
+		return;
+	}
+
+	if (DecrementCounter(state.m_nReplaceCountdown)) {
+		pMonster->Delete();
+		CreateMonster();
+	}
+}
+
+void CRangda::ProcessGenerate() {
+	if (DecrementCounter(state.m_nGenerateCountdown)) {
+		CreateMonster();
+	}
+}
+
+bool CRangda::DecrementCounter(int & tickCounter) {
+	if (tickCounter > 0)
+		tickCounter--;
+	return tickCounter == 0;
+}
+
+////////
+
+CRangda::RANGDA_POS CRangda::Prop::GetRandomPos(std::optional<size_t> & oldPos) const {
+	ASSERT(!m_vvPos.empty());
+
+	if (m_vvPos.size() == 1) {
+		oldPos = 0;
 		return m_vvPos[0];
-	
-	int nPos;
-	do	{	// 출력 좌표는 기존 좌표와 달라야 한다
-		nPos	= xRandom( 0, m_vvPos.size() );
-	}	while( nPos == m_nOldPos );
+	}
 
-	m_nOldPos	= nPos;
+	size_t nPos;
+	do {
+		nPos = xRandom(0, m_vvPos.size());
+	} while (oldPos && nPos == *oldPos);
+
+	oldPos = nPos;
 	return m_vvPos[nPos];
 }
 
-void CRangda::SetObj( OBJID objid )
-{	// 현재 생성된 실 객체를 설정
-	m_objid		= objid;
-	m_nGenerateCountdown	= m_nInterval;
-	m_nReplaceCountdown		= m_nReplace;
-	m_bReplaceable	= TRUE;
-}
-
-void CRangda::OnTimer( void )
-{
-	CMover* pMonster	= GetMonster();
-	if( IsValidObj( pMonster ) )	// 객체가 존재하면
-		ProcessReplace( pMonster );		// 조건을 만족하면 임의의 좌표로 이동시킨다
-	else	// 객체가 존재하지 않으면 재생
-		ProcessGenerate();
-}
-
-CMover* CRangda::GetMonster( void )
-{	// 실 객체의 포인터를 반환
-	if( m_objid != NULL_ID )
-	{
-		CMover* pMonster	= prj.GetMover( m_objid );
-		if( IsValidObj( pMonster ) )
-			return pMonster;
-		m_objid		= NULL_ID;
-	}
-	return NULL;
-}
-
-void CRangda::ProcessReplace( CMover* pMonster )
-{	// 임의의 좌표로 이동
-	if( m_bReplaceable )
-	{
-		if( IsDamaged( pMonster ) )		// 상처 입었으면
-			m_bReplaceable	= FALSE;	// 이동시키지 않는다
-		else if( HavetoReplace() )	// 이동할 조건을 만족하면
-		{
-			election::OutputDebugString( "CRangda.ProcessReplace:" );
-			pMonster->Delete();		// 제거하고
-			CreateMonster();	// 새로 생성한다
-		}
-	}
-}
-
-void CRangda::ProcessGenerate( void )
-{
-	if( HavetoGenerate() )	// 재생 조건을 만족하면
-	{
-		election::OutputDebugString( "CRangda.ProcessGenerate:" );
-		CreateMonster();	// 새로 생성한다
-	}
-}
-
-BOOL CRangda::IsDamaged( CMover* pMonster )
-{	// 상처 입었나?
-	return ( pMonster->GetHitPoint() < pMonster->GetMaxHitPoint() );
-}
-
-BOOL CRangda::HavetoReplace( void )
-{	// 좌표를 바꿀 시간이 되었나?
-	if( m_nReplaceCountdown > 0 )
-		m_nReplaceCountdown--;
-	return ( m_nReplaceCountdown == 0 );
-}
-
-BOOL CRangda::HavetoGenerate( void )
-{	// 재생 시간이 되었나?
-	if( m_nGenerateCountdown > 0 )
-		m_nGenerateCountdown--;
-	return ( m_nGenerateCountdown == 0 );
-}
-
-void CRangda::CreateMonster( void )
-{	// 몬스터 생성
-	RANGDA_POS pos	= GetRandomPos();
-	CWorld* pWorld	= g_WorldMng.GetWorld( pos.dwWorldId );
-	if( !pWorld )
-		return;
-	CMover* pMonster	= static_cast<CMover*>( ::CreateObj( OT_MOVER, m_dwMonster ) );
-	if( pMonster )
-	{
-		pMonster->m_bActiveAttack	= m_bActiveAttack;
-		pMonster->SetPos( pos.vPos );
-		pMonster->InitMotion( MTI_STAND );
-		pMonster->UpdateLocalMatrix();
-		pMonster->AddItToGlobalId();
-		pWorld->ADDOBJ( pMonster, FALSE, nDefaultLayer );
-		SetObj( pMonster->GetId() );
-		election::OutputDebugString( "CRangda.CreateMonster: monster = %d, x = %0.2f, y = %0.2f, z = %0.2f\n",
-			pMonster->GetIndex(), pMonster->GetPos().x, pMonster->GetPos().y, pMonster->GetPos().z );
-	}
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 // CRangdaController
@@ -152,13 +131,11 @@ bool CRangdaController::LoadScript(const char * szFile) {
 	CScript s;
 	if (!s.Load(szFile)) return false;
 
-
 	while (true) {
 		const DWORD dwMonster = s.GetNumber();		// subject or FINISHED
-
 		if (s.tok == FINISHED) break;
 
-		CRangda & pRangda = m_vRangda.emplace_back(dwMonster);
+		CRangda::Prop prop{ dwMonster };
 
 		s.GetToken();	// {
 		while (true) {
@@ -166,11 +143,11 @@ bool CRangdaController::LoadScript(const char * szFile) {
 			if (*s.token == '}') break;
 
 			if (s.Token == _T("nInterval"))
-				pRangda.SetInterval(s.GetNumber());
+				prop.m_nInterval = s.GetNumber();
 			else if (s.Token == _T("nReplace"))
-				pRangda.SetReplace(s.GetNumber());
+				prop.m_nReplace = s.GetNumber();
 			else if (s.Token == _T("bActiveAttack"))
-				pRangda.SetActiveAttack(static_cast<BOOL>(s.GetNumber()));
+				prop.m_bActiveAttack = static_cast<BOOL>(s.GetNumber());
 			else if (s.Token == _T("vRangda")) {
 				s.GetToken();	// {
 
@@ -184,10 +161,12 @@ bool CRangdaController::LoadScript(const char * szFile) {
 					pos.vPos.y = s.GetFloat();
 					pos.vPos.z = s.GetFloat();
 
-					pRangda.AddPos(pos);
+					prop.m_vvPos.emplace_back(pos);
 				}
 			}
 		}
+
+		m_vRangda.emplace_back(prop);
 	}
 
 	return true;
--- Source/WORLDSERVER/rangda.h
+++ Source/WORLDSERVER/rangda.h
@@ -1,48 +1,53 @@
 #pragma once
 
-
-// 단일 랜덤 이벤트 몬스터를 제어하는 클래스
-class CRangda final
-{
+// Controls a single Rangda
+class CRangda final {
 public:
-	// 랜덤 이벤트 몬스터가 나타나는 지점을 나타내는 구조체
+	// A world - position pair
 	struct RANGDA_POS final {
-		DWORD	dwWorldId;	// 나타날 수 있는 곳의 월드 식별자
-		D3DXVECTOR3 vPos;	// 나타날 수 있는 곳의 좌표
+		DWORD	dwWorldId;
+		D3DXVECTOR3 vPos;
 	};
 
+	struct Prop {
+		explicit Prop(DWORD dwMonster) : m_dwMonster(dwMonster) {}
 
-	CRangda( DWORD dwMonster );
+		DWORD	m_dwMonster;                // Monster id
+		int		m_nInterval = 0;	          // Time before spawning when dead
+		int		m_nReplace = 0;		          // Time before moving if alive and undamage
+		BOOL	m_bActiveAttack = FALSE;	  // Is red monster?
+		std::vector<RANGDA_POS>	m_vvPos; // Random positions where it can appear
 
-	void	AddPos( const RANGDA_POS & vPos );	// 좌표 추가
-	void	OnTimer();	// 틱 처리
-	void	SetInterval( int nInterval )	{	m_nInterval	= nInterval;	}	// 재생 간격 설정
-	void	SetReplace( int nReplace )	{	m_nReplace	= nReplace;		}	// 이동 간격 설정
-	void	SetActiveAttack( BOOL bActiveAttack )	{	m_bActiveAttack		= bActiveAttack;	}	// 선공 여부 설정
+		// Randomly return one of the positions and update oldPos. The new position
+		// is different that the one stored in oldPos.
+		[[nodiscard]] RANGDA_POS GetRandomPos(std::optional<size_t> & oldPos) const;
+	};
+
+	explicit CRangda(Prop prop);
+
+	void	OnTimer();
 
 private:
-	RANGDA_POS	GetRandomPos( void );	// 임의 좌표 반환
-	void	SetObj( OBJID objid );		// 실 객체 설정
-	BOOL	HavetoGenerate( void );		// 재생 조건을 만족하는가?
-	BOOL	HavetoReplace( void );		// 이동 조건을 만족하는가?
-	void	CreateMonster( void );	// 실 객체 생성
-	BOOL	IsDamaged( CMover* pMonster );	// 상처 입었나?
-	void	ProcessReplace( CMover* pMonster );		// 이동 처리
-	void	ProcessGenerate( void );	// 재생 처리
-	CMover*		GetMonster( void );		// 실 객체 반환
+	void SetObj(OBJID objid);
+	void CreateMonster();
+	[[nodiscard]] CMover * GetMonster();
+	void ProcessReplace(CMover * pMonster);
+	void ProcessGenerate();
+
+	static bool IsDamaged(CMover * pMonster);
+	static bool DecrementCounter(int & tickCounter); // Decrement tickCounter towards 0 and return true if reached 0
+
 private:
-	const DWORD	m_dwMonster;	// 생성할 몬스터 종류 식별자
-	int		m_nInterval;	// 재생 간격
-	int		m_nReplace;		// 이동 간격
-	BOOL	m_bActiveAttack;	// 선공 여부
-	std::vector<RANGDA_POS>	m_vvPos;	// 나타날 수 있는 좌표 집합
-	int		m_nGenerateCountdown;	// 재생 대기 시간 틱당 1 감소
-	int		m_nReplaceCountdown;	// 이동 대기 시간, 틱당 1 감소
-	BOOL	m_bReplaceable;		// 이동 가능 플래그
-	int		m_nOldPos;		// 이전 좌표
-	OBJID	m_objid;	// 실 객체 식별자
+	/* const */ Prop prop;
+
+	struct State {
+		int  m_nGenerateCountdown = 0;	// Time before spawning, decreased by 1 at each tick
+		int  m_nReplaceCountdown = 0;   // Time before re-placing, decreased by 1 at each tick
+		bool m_bReplaceable = true;     // If false, prevent re-placing
+		std::optional<size_t> m_nOldPos = std::nullopt;
+		OBJID	m_objid = NULL_ID;
+	} state;
 };
-
 
 class CRangdaController final {
 public:
@@ -56,4 +61,3 @@ private:
 private:
 	std::vector<CRangda> m_vRangda;
 };
-
