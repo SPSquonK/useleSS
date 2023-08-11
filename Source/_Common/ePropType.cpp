dd0e67dbe2039d95d490ec05efae3c9813924539
SquonK <sp@sqk.fr>
2023-08-12 00:20:02 +0200

    Use the proper ePropType prefix in ePropType.cpp

--- Source/_Common/ePropType.cpp
+++ Source/_Common/ePropType.cpp
@@ -4,55 +4,55 @@
 
 DWORD SAI79::GetAttackStone(ePropType element) {
 	switch (element) {
-		case SAI79::FIRE:        return II_CHR_SYS_SCR_FIREASTONE;
-		case SAI79::WATER:       return II_CHR_SYS_SCR_WATEILSTONE;
-		case SAI79::WIND:        return II_CHR_SYS_SCR_WINDYOSTONE;
-		case SAI79::ELECTRICITY: return II_CHR_SYS_SCR_LIGHTINESTONE;
-		case SAI79::EARTH:       return II_CHR_SYS_SCR_EARTHYSTONE;
-		default:                 return 0;
+		case ePropType::FIRE:        return II_CHR_SYS_SCR_FIREASTONE;
+		case ePropType::WATER:       return II_CHR_SYS_SCR_WATEILSTONE;
+		case ePropType::WIND:        return II_CHR_SYS_SCR_WINDYOSTONE;
+		case ePropType::ELECTRICITY: return II_CHR_SYS_SCR_LIGHTINESTONE;
+		case ePropType::EARTH:       return II_CHR_SYS_SCR_EARTHYSTONE;
+		default:                     return 0;
 	}
 }
 
 DWORD SAI79::GetDefenseStone(ePropType element) {
 	switch (element) {
-		case SAI79::FIRE:        return II_CHR_SYS_SCR_DEFIREASTONE;
-		case SAI79::WATER:       return II_CHR_SYS_SCR_DEWATEILSTONE;
-		case SAI79::WIND:        return II_CHR_SYS_SCR_DEWINDYOSTONE;
-		case SAI79::ELECTRICITY: return II_CHR_SYS_SCR_DELIGHTINESTONE;
-		case SAI79::EARTH:       return II_CHR_SYS_SCR_DEEARTHYSTONE;
-		default:                 return 0;
+		case ePropType::FIRE:        return II_CHR_SYS_SCR_DEFIREASTONE;
+		case ePropType::WATER:       return II_CHR_SYS_SCR_DEWATEILSTONE;
+		case ePropType::WIND:        return II_CHR_SYS_SCR_DEWINDYOSTONE;
+		case ePropType::ELECTRICITY: return II_CHR_SYS_SCR_DELIGHTINESTONE;
+		case ePropType::EARTH:       return II_CHR_SYS_SCR_DEEARTHYSTONE;
+		default:                     return 0;
 	}
 }
 
 std::optional<std::pair<SAI79::ePropType, SAI79::StoneType>> SAI79::StoneIdToElement(DWORD stoneID) {
 	using P = std::pair<SAI79::ePropType, SAI79::StoneType>;
 	switch (stoneID) {
-		case II_CHR_SYS_SCR_FIREASTONE:      return P{ SAI79::FIRE       , StoneType::Attack  };
-		case II_CHR_SYS_SCR_DEFIREASTONE:    return P{ SAI79::FIRE       , StoneType::Defense };
-		case II_CHR_SYS_SCR_WATEILSTONE:     return P{ SAI79::WATER      , StoneType::Attack  };
-		case II_CHR_SYS_SCR_DEWATEILSTONE:   return P{ SAI79::WATER      , StoneType::Defense };
-		case II_CHR_SYS_SCR_WINDYOSTONE:     return P{ SAI79::WIND       , StoneType::Attack  };
-		case II_CHR_SYS_SCR_DEWINDYOSTONE:   return P{ SAI79::WIND       , StoneType::Defense };
-		case II_CHR_SYS_SCR_LIGHTINESTONE:   return P{ SAI79::ELECTRICITY, StoneType::Attack  };
-		case II_CHR_SYS_SCR_DELIGHTINESTONE: return P{ SAI79::ELECTRICITY, StoneType::Defense };
-		case II_CHR_SYS_SCR_EARTHYSTONE:     return P{ SAI79::EARTH      , StoneType::Attack  };
-		case II_CHR_SYS_SCR_DEEARTHYSTONE:   return P{ SAI79::EARTH      , StoneType::Defense };
+		case II_CHR_SYS_SCR_FIREASTONE:      return P{ ePropType::FIRE       , StoneType::Attack  };
+		case II_CHR_SYS_SCR_DEFIREASTONE:    return P{ ePropType::FIRE       , StoneType::Defense };
+		case II_CHR_SYS_SCR_WATEILSTONE:     return P{ ePropType::WATER      , StoneType::Attack  };
+		case II_CHR_SYS_SCR_DEWATEILSTONE:   return P{ ePropType::WATER      , StoneType::Defense };
+		case II_CHR_SYS_SCR_WINDYOSTONE:     return P{ ePropType::WIND       , StoneType::Attack  };
+		case II_CHR_SYS_SCR_DEWINDYOSTONE:   return P{ ePropType::WIND       , StoneType::Defense };
+		case II_CHR_SYS_SCR_LIGHTINESTONE:   return P{ ePropType::ELECTRICITY, StoneType::Attack  };
+		case II_CHR_SYS_SCR_DELIGHTINESTONE: return P{ ePropType::ELECTRICITY, StoneType::Defense };
+		case II_CHR_SYS_SCR_EARTHYSTONE:     return P{ ePropType::EARTH      , StoneType::Attack  };
+		case II_CHR_SYS_SCR_DEEARTHYSTONE:   return P{ ePropType::EARTH      , StoneType::Defense };
 		default:                             return std::nullopt;
 	}
 }
 
 DWORD SAI79::GetResistDST(const ePropType element) {
 	switch (element) {
-		case SAI79::FIRE:
+		case ePropType::FIRE:
 		default:
 			return DST_RESIST_FIRE;
-		case SAI79::WATER:
+		case ePropType::WATER:
 			return DST_RESIST_WATER;
-		case SAI79::WIND:
+		case ePropType::WIND:
 			return DST_RESIST_WIND;
-		case SAI79::ELECTRICITY:
+		case ePropType::ELECTRICITY:
 			return DST_RESIST_ELECTRICITY;
-		case SAI79::EARTH:
+		case ePropType::EARTH:
 			return DST_RESIST_EARTH;
 	}
 }
@@ -61,12 +61,12 @@ DWORD SAI79::GetEleCard(const ePropType element) {
 	// 속성 제련 용 카드의 종류가 
 	// 속성 당 하나로 통합됨
 	switch (element) {
-		case SAI79::FIRE:        return II_GEN_MAT_ELE_FLAME;
-		case SAI79::WATER:       return II_GEN_MAT_ELE_RIVER;
-		case SAI79::ELECTRICITY: return II_GEN_MAT_ELE_GENERATOR;
-		case SAI79::WIND:        return II_GEN_MAT_ELE_CYCLON;
-		case SAI79::EARTH:       return II_GEN_MAT_ELE_DESERT;
-		default:                 return 0;
+		case ePropType::FIRE:        return II_GEN_MAT_ELE_FLAME;
+		case ePropType::WATER:       return II_GEN_MAT_ELE_RIVER;
+		case ePropType::ELECTRICITY: return II_GEN_MAT_ELE_GENERATOR;
+		case ePropType::WIND:        return II_GEN_MAT_ELE_CYCLON;
+		case ePropType::EARTH:       return II_GEN_MAT_ELE_DESERT;
+		default:                     return 0;
 	}
 }
 
