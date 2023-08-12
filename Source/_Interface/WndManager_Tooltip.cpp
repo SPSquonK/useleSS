8451af9b960e9e9e21a1355bfdbbb41f677b2a72
SquonK <sp@sqk.fr>
2023-08-12 03:54:58 +0200

    Display again BaseElementResist of items
    Reverting ffb0aa75050e48d90436a8976cca616e9d16b753 as
    DST_RESIST_[ELEM] stats actually work: they are used when a
    skill is used on the mover.
    
    It means that:
    - 991a53db461459b2645d537cb4060de5ab52223f actually fixes something
    Here is a video with the bug (played on the commit before 991a53db461459b2645d537cb4060de5ab52223f)
    https://youtu.be/szYw6wtiheQ
    
    - 2808cd382c1db519c811a1067a26f4bf01f40987 is actually meaningful
    for better comprehension

--- Source/_Interface/WndManager_Tooltip.cpp
+++ Source/_Interface/WndManager_Tooltip.cpp
@@ -919,26 +919,24 @@ namespace WndMgr {
 	}
 
 	void CTooltipBuilder::PutBaseResist(const ItemProp & pItemProp, CEditString & pEdit, bool isIgnored) const {
-		if constexpr (false /* DST_RESIST_[ELEM_NAME] stats are active == false */) {
-			const ToolTipItemTextColor & colors = dwItemColor[g_Option.m_nToolTipText];
+		const ToolTipItemTextColor & colors = dwItemColor[g_Option.m_nToolTipText];
 
-			using BaseResist = std::tuple<DWORD, int, DWORD>;
+		using BaseResist = std::tuple<DWORD, int, DWORD>;
 
-			const std::array<BaseResist, 5> values{
-				BaseResist(TID_GAME_TOOLTIP_ELEC_RES , pItemProp.nItemResistElecricity, colors.dwResistElectricity),
-				BaseResist(TID_GAME_TOOLTIP_FIRE_RES , pItemProp.nItemResistFire      , colors.dwResistFire),
-				BaseResist(TID_GAME_TOOLTIP_WATER_RES, pItemProp.nItemResistWater     , colors.dwResistWater),
-				BaseResist(TID_GAME_TOOLTIP_WIND_RES , pItemProp.nItemResistWind      , colors.dwResistWind),
-				BaseResist(TID_GAME_TOOLTIP_EARTH_RES, pItemProp.nItemResistEarth     , colors.dwResistEarth)
-			};
+		const std::array<BaseResist, 5> values{
+			BaseResist(TID_GAME_TOOLTIP_ELEC_RES , pItemProp.nItemResistElecricity, colors.dwResistElectricity),
+			BaseResist(TID_GAME_TOOLTIP_FIRE_RES , pItemProp.nItemResistFire      , colors.dwResistFire),
+			BaseResist(TID_GAME_TOOLTIP_WATER_RES, pItemProp.nItemResistWater     , colors.dwResistWater),
+			BaseResist(TID_GAME_TOOLTIP_WIND_RES , pItemProp.nItemResistWind      , colors.dwResistWind),
+			BaseResist(TID_GAME_TOOLTIP_EARTH_RES, pItemProp.nItemResistEarth     , colors.dwResistEarth)
+		};
 
-			CString strTemp;
-			for (const auto & [tooltipId, baseResistAdj, color] : values) {
-				if (baseResistAdj != 0) {
-					strTemp.Format(prj.GetText(tooltipId), baseResistAdj);
-					pEdit.AddString("\n");
-					pEdit.AddString(strTemp, color, isIgnored ? ESSTY_STRIKETHROUGH : 0);
-				}
+		CString strTemp;
+		for (const auto & [tooltipId, baseResistAdj, color] : values) {
+			if (baseResistAdj != 0) {
+				strTemp.Format(prj.GetText(tooltipId), baseResistAdj);
+				pEdit.AddString("\n");
+				pEdit.AddString(strTemp, color, isIgnored ? ESSTY_STRIKETHROUGH : 0);
 			}
 		}
 	}
